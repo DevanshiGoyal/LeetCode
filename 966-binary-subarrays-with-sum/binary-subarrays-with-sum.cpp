e27a52c9160a64d9ea968115915731e221fdffc9
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-09-07 23:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (38.63%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int SubarraySumLessOrEqual(vector<int>& nums, int goal){
+        if (goal<0) return 0;
+        int sum = 0;
+        int l =0;
+        int r = 0;
+        int count = 0;
+
+        while(r<nums.size()){
+            sum = sum + nums[r];
+            while (sum > goal){
+                sum = sum - nums[l];
+                l++;
+            }
+            count = count + (r-l+1);
+            r++;
+        }
+        return count ;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return SubarraySumLessOrEqual(nums , goal) - SubarraySumLessOrEqual(nums , goal -1);
+    }
+};
+
+// TC---> O(2*2N)----> O(N)
+//SC---> O(1)
+
+      
