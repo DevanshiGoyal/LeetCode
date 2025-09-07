87458a9078ef4e9f7dc4620651028d25314dcef2
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-09-07 23:40:22 +0530

    Time: 12 ms (38.10%) | Memory: 71.4 MB (68.58%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        int oddCount = 0;   // count of odds in current window
-        int count = 0;      // total number of nice subarrays
-        int prevCount = 0;  // number of valid left shifts (starting positions)
-        int i = 0, j = 0;   // two pointers (window boundaries)
-
-        while (j < n) {
-            // If nums[j] is odd, increase odd count and reset prevCount
-            if (nums[j] % 2 != 0) { 
-                oddCount++;
-                prevCount = 0; 
-            }
-
-            // If window has exactly k odds
-            while (oddCount == k) {
-                prevCount++; // each left shift gives a new valid subarray
-                if (nums[i] % 2 != 0) oddCount--; // reduce odd count if removing odd
-                i++; // shrink window from left
-            }
-
-            // Add all valid subarrays ending at j
-            count += prevCount; 
-            j++;
+        return atMost(nums, k) - atMost(nums, k - 1); 
+    }
+    
+    int atMost(vector<int>& nums, int k) {
+        int s = 0, ans = 0;
+        for(int i = 0, j = 0; i < nums.size(); i++) {
+            s += nums[i] % 2;
+            while(s > k) s -= (nums[j++] % 2);
+            ans += (i - j + 1);
         }
-
-        return count;
+        return ans;
     }
 };
