a6960fc224d3c3bd20ca9f1277deafca3425864b
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-02 23:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (17.17%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -56,15 +56,35 @@ public:
 
     }
 
+    int tabulationSpaceOptimized(int m, int n) { 
+        // TC : O(M * N)
+        // SC : O(N)
+        vector<int> prev(n , 0); //Prev row of size number of columns
+        for (int i = 0; i < m; i++) {
+            vector<int> curr(n , 0); // current row 
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) curr[j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if (i > 0) up = prev[j];
+                    if (j > 0) left = curr[j - 1];
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n - 1];
 
-
+    }
 
     int uniquePaths(int m, int n) {
 
-        //return recursion(m-1 , n-1) ;
         vector<vector<int>>dp(m , vector<int>(n , -1)) ;
+        //return recursion(m-1 , n-1) ;
+        
         //return memoization(m - 1, n - 1, dp);
-        return tabulation(m, n, dp);
+        //return tabulation(m, n, dp);
+        return tabulationSpaceOptimized(m, n);
         
     }
 };
