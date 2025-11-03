b2535a9abd628ca64dcde8bf9b7e808e8a0621cb
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-03 22:40:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (72.36%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -50,6 +50,40 @@ public:
         return dp[i][j] = up + left ;
 
     }
+
+    // Tabulation
+    int  tabulation(vector<vector<int>>& obstacleGrid , vector<vector<int>> &dp, int m , int n ){
+        // base condition 
+
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0 ;
+                }
+                else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+                else{
+                    int up = 0 ; 
+                    int left = 0 ;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j > 0){
+                        left = dp[i][j-1]; 
+                    }
+                    dp[i][j] = up + left;
+        
+
+                }
+            }
+        
+        }
+                
+        return dp[m-1][n-1];
+
+    } 
+
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
@@ -57,7 +91,9 @@ public:
         vector<vector<int>> dp(m , vector<int>(n , -1)) ;
         //return recursion(m-1 , n-1 , obstacleGrid );
 
-        return memoization(m-1 , n-1 , obstacleGrid , dp );
+        //return memoization(m-1 , n-1 , obstacleGrid , dp );
+
+        return tabulation(obstacleGrid , dp  , m ,n);
 
 
         
@@ -80,3 +116,11 @@ public:
 
 //Sc: O(m * n)
 //DP table + recursion stack
+
+//Tabulation 
+
+//Time Complexity: O(m * n)
+//Iterates over full DP table
+
+//Space Complexity: O(m * n)
+//Full DP table
