7bf56be16c6d99fbe275e5b38d6115e397643ffe
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-05 19:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (59.18%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -29,13 +29,35 @@ public:
 
     }
 
+    //Tc: O(n*m)
+    //Sc: O(n*m)
+
+    int  tabulation(vector<vector<int>> &cost,int m, int n , vector<vector<int>>& dp){
+
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(i == 0 && j == 0) dp[i][j] =cost[i][j] ;
+                else{
+                    int up = INT_MAX , left = INT_MAX ;
+                    if(i>0) up = dp[i-1][j] ;
+                    if(j>0) left = dp[i][j-1];
+
+                    dp[i][j] = cost[i][j] + min(up  , left);
+                    
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     
 
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         //return minCost(grid,m-1,n-1);
         vector<vector<int>> dp(m , vector<int> (n , -1));
-        return memoization(grid,m-1,n-1 , dp);
+        //return memoization(grid,m-1,n-1 , dp);
+        return tabulation(grid,m , n , dp);
         
     }
 };
