b7e06b87dcf9e0776785ac66f9af084787a7e0db
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-13 02:12:11 +0530

    Time: 26 ms (62.14%) | Memory: 22 MB (49.85%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -9,6 +9,8 @@ public:
     //memoization 
     // tc --- > O(N*T) 
     // Sc --- > O(N*T) + O(N) 
+
+
     int minCoins(int  ind , int target , vector<int>& coins , vector<vector<int>> &dp){
         // base condition
         if( ind == 0 ){
@@ -27,13 +29,35 @@ public:
 
     }
 
+    // space optimisation 
+     // tc---> O(N*amount)
+     //sc--> O(amount)
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size() ;
-        vector<vector<int>> dp(n , vector<int>(amount + 1 , -1)) ;
+        vector<int> prev(amount+1 , 0) , curr(amount + 1 , 0) ;
+
+        for(int T = 0 ; T<=amount ; T++){
+            if(T % coins[0] == 0) prev[T] =  T/coins[0] ;
+            else prev[T] = 1e9;
+        }
+
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int T = 0 ; T <=amount ; T++){
+
+                int notPick = 0 + prev[T] ;
+                int pick = INT_MAX ;
+                if(coins[ind] <= T) pick = 1 + curr[T - coins[ind]] ;
+
+                curr[T] = min(pick , notPick) ;
+
+            }
+            prev = curr ;
+        }
 
         
-        int ans = minCoins(n - 1, amount, coins , dp);
+        //int ans = minCoins(n - 1, amount, coins , dp);
+        int ans = prev[amount] ;
         if (ans >= 1e9) return -1;  // convert "impossible" to -1
         return ans;
         
