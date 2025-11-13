4df32fe48d47ef39ec56b2782fee7da08ec23884
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-14 01:50:26 +0530

    Time: 26 ms (62.22%) | Memory: 36.5 MB (54.80%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int memo(int amount,int ind,vector<int>&coins){
-        if(amount == 0)return 1;
-        if(ind < 0 || amount < 0)return 0;
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        
-        int pick = memo(amount - coins[ind],ind,coins);
-        int notpick = memo(amount,ind-1,coins);
+    int minCoins(int  ind , int target , vector<int>& coins , vector<vector<int>> &dp){
+        // base condition
+        if( ind == 0 ){
+            if(target % coins[0] == 0) return 1 ;
+            else return 0 ;
+        }
+        if(dp[ind][target] != -1) return dp[ind][target] ;
 
-        return dp[ind][amount] = pick + notpick;
+        // for infinite supply / multiple use 
+        // pick will stand on the same ind 
+        int notPick = minCoins(ind-1 , target , coins  , dp);
+        int pick = 0 ;
+        if(coins[ind] <= target) pick = minCoins(ind , target - coins[ind] , coins , dp) ;
+
+        return dp[ind][target] = pick + notPick ;
+
     }
+
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        dp.resize(n+1,vector<int>(amount+1,-1));
-        return memo(amount,n-1,coins);
+        
+
+        int n = coins.size() ;
+        vector<vector<int>> dp(n , vector<int>(amount + 1 , -1)) ;
+
+        int ans = minCoins(n - 1, amount, coins , dp);
+        
+        
+        return ans;
+
+    
     }
 };
