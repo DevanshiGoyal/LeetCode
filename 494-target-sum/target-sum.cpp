0a87362b98ca1730b17392d43aca9b13f01c71ae
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-13 21:41:09 +0530

    Time: 796 ms (16.35%) | Memory: 11.6 MB (91.91%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // recursion
+
+    int totalWays = 0;
+    void calculateWays(vector<int>& nums, int currentIndex, int currentSum,
+                       int target) {
+        if (currentIndex == nums.size()) {
+            // Check if the current sum matches the target
+            if (currentSum == target) {
+                totalWays++;
+            }
+        } else {
+            // Include the current number with a positive sign
+            calculateWays(nums, currentIndex + 1,
+                          currentSum + nums[currentIndex], target);
+            // Include the current number with a negative sign
+            calculateWays(nums, currentIndex + 1,
+                          currentSum - nums[currentIndex], target);
+        }
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        
+        calculateWays(nums , 0 , 0 ,target ) ;
+        return totalWays ;
+       
+    }
+};
