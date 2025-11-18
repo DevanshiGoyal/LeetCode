39278ea20234cb95aff2319dc257b72cd136549c
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-19 02:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (71.46%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
-        int i = 0;
-        while(i<bits.size()-1)
-        {
-            if(bits[i] == 0 )
-                i++;
-            else
-                i+=2;
+        const int n=bits.size();
+        int i=0;
+        for(; i<n-1; i++){ // Proceed the loop from i=0 to n-2
+            if(bits[i]==1) i++; // the valid segment "10" or "11"
         }
-        if(i==bits.size())
-            return 0;
-        else
-            return 1;  
+        return i==n-1; // if the last  valid segment is "10" or "11" i==n otherwise i==n-1
     }
-
 };
