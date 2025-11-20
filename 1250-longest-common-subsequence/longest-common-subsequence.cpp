c0a0f4c12e00fcfb38808a6496a280a5ccd40e6c
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-21 02:43:10 +0530

    Time: 43 ms (23.64%) | Memory: 27.8 MB (5.95%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    // Recursion ---> TLE 
-    // Memoization 
-    int lcsUtil(string& s1, string& s2, int ind1, int ind2 , vector<vector<int>> &dp) {
-    // Base case 
-    // index become -ve , return 0
-    if (ind1 < 0 || ind2 < 0)
-        return 0;
-
-    //  if result for this pair of indices is already calculated, return it
-    if (dp[ind1][ind2] != -1)
-        return dp[ind1][ind2];
-
-    // If the char at the current indices *match*, increment the LCS length
-    if (s1[ind1] == s2[ind2])
-        return dp[ind1][ind2] =  1 + lcsUtil(s1, s2, ind1 - 1, ind2 - 1 , dp);
-    else
-        // If the characters don't match
-        // then 2 condition ---> (ind1-1 , ind2 ) and  (ind1 , ind2 -1 )
-        return dp[ind1][ind2] = max(lcsUtil(s1, s2, ind1, ind2 - 1 , dp), lcsUtil(s1, s2, ind1 - 1, ind2 , dp));
-}
+    // by shifting of index
+    // Memoization
+    int lcsUtil(string &s1, string &s2,int i,int j,vector<vector<int>> &memo) {
+        if(i==0 || j==0){ // negative index
+            return 0;
+        }
+        if(memo[i][j]!=-1){
+            return memo[i][j];
+        }
+        if(s1[i-1] == s2[j-1]){
+            return memo[i][j]= 1+lcsUtil(s1,s2,i-1,j-1,memo);
+        }else{
+            return memo[i][j]= max(lcsUtil(s1,s2,i-1,j,memo) , lcsUtil(s1,s2,i,j-1,memo));
+        }
+    }
+    
     int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.size() , n=text2.size();
-        vector<vector<int>> dp(m , vector<int>(n , -1))  ;
-        return lcsUtil(text1,text2,m-1,n-1 , dp );
-        
-        
+        int m=text1.size(),n=text2.size();
+      
+        vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1))  ;
+        return lcsUtil(text1,text2, m ,n, dp );
     }
 };
