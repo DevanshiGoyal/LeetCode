59380d173cfe7143e00c2ee4640991b4e7cf3226
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-11-21 03:13:12 +0530

    Time: 15 ms (91.13%) | Memory: 9.1 MB (92.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     // shifting of index by 1 
-    // tabulation 
+    // space optimization 
     // tc--->O(n*m)
-    // sc--->O(n*m)
+    // sc--->O(m)
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // Create a DP table
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
         // base cases
         for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j <= m; j++) {
-            dp[0][j] = 0;
+            prev[0] = 0;
         }
 
         // fill in the DP table to calc the length of LCS
         for (int ind1 = 1; ind1 <= n; ind1++) {
             for (int ind2 = 1; ind2 <= m; ind2++) {
                 if (text1[ind1 - 1] == text2[ind2 - 1])
-                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
+                    curr[ind2] = 1 + prev[ind2 - 1]; // Characters match, increment LCS length
                 else
-                    dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match
+                    curr[ind2] = max(prev[ind2], curr[ind2 - 1]); // Characters don't match
             }
+            prev = curr ;
         }
 
-        return dp[n][m]; // Return the length of the Longest Common Subsequence
+        return prev[m]; // Return the length of the Longest Common Subsequence
             
     }
 };
