fbb1dd4edbb98744259abf400e5c40cd87083b22
Devanshi Goyal <goyaldevanshi444@gmail.com>
2025-12-08 00:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (38.00%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int count = 0 ;
-        for(int i = low ; i<=high ; i++){
-            if(i%2 != 0) count ++ ;
-        }
-        return count ;
+        //using maths trick 
+        // tc--> O(1)
+        // (x + 1) // 2   odd [1----- x]
+        // [low , high]
+        //odds up to high   −   odds up to (low - 1)
+        int odds = (high + 1) / 2  - (low /2) ;
+        return odds ;
         
     }
 };
