95f124283720c0713481a41a1794b31c99f4cb98
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-01-09 23:50:40 +0530

    Time: 1 ms (79.47%) | Memory: 10.1 MB (76.79%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> value = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
 
-        int ans = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            // if current value is smaller than next value → subtract
-            if (i + 1 < s.length() && value[s[i]] < value[s[i + 1]]) {
-                ans -= value[s[i]];
-            }
-            // Else add
-            else {
-                ans += value[s[i]];
-            }
-        }
-
-        return ans;
+    int priority(char c){
+        if(c=='I') return 1;
+        if(c=='V') return 5;
+        if(c=='X') return 10;
+        if(c=='L') return 50;
+        if(c=='C') return 100;
+        if(c=='D') return 500;
+        if(c=='M') return 1000;
+        return 0;
     }
-};
 
-//tc--> o(n)
-//sc--> O(1) constant size
+    int romanToInt(string s) {
+        int sol = 0;
+        for(int i=1; i<s.size(); i++){
+            // largest to smallest 
+            if(priority(s[i-1])>=priority(s[i])) sol += priority(s[i-1]);
+            else sol -= priority(s[i-1]);
+        }  
+        sol += priority(s[s.size()-1]);
+        return sol;      
+    }
+
+    
+};
+//TC-->O(N)  SC-->O(1)
