7bbbddd8efae471166ea85509fb0b9648d3d884c
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-01-22 22:24:57 +0530

    Time: 31 ms (33.04%) | Memory: 139.1 MB (66.73%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,56 +1,44 @@
+/*
+using deque approach   ---> montonic decreasing 
+1  When new ele comes nums[i] , make space fo fit (window size can't be greater than k )
+2  when nums[i] > deque ele no need to keep small ele in that window , pop them
+
+3  push i in deque ---> for nums[i]
+
+4  if(i>= k-1) , deq.front() is oue ans for that window 
+
+*/
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-
         int n = nums.size() ;
-        priority_queue<pair<int , int>> pq ; // {(nums[i] , i)};
 
-        vector<int> ans(n-k+1) ; // no of windows n-k+1
-        // TC---> O(k ·log k)
-        for(int i = 0 ; i<k ; i++){
-            pq.push({nums[i] , i}) ;
-        }
+        deque<int> deq;
 
-        ans[0] = pq.top().first ;
-        //(n-k)
-        for(int i = k ; i<n ; i++){
-            while(!pq.empty() && pq.top().second <=(i-k)){ // just outside the window
-                pq.pop() ; // remove the ele not in window 
+        vector<int> result ;
+
+        for(int i = 0 ; i<n ; i++){
+            // make space for nums[i] 
+            // <=i-k
+            while(!deq.empty() && deq.front() <= i-k){
+                deq.pop_front() ;
             }
-            pq.push({nums[i] , i});
-            ans[i-k+1] = pq.top().first ;
-        }
+            // no need to keep small ele --> monotonic decreasing 
+            while(!deq.empty() && nums[i] >= nums[deq.back()]){
+                deq.pop_back();
 
-        return ans ;
+            }
+            deq.push_back(i) ;
+
+            // add max for current window
+            if(i>=k-1){
+                result.push_back(nums[deq.front()]) ;
+            }
+
+        }
+        return result ;        
         
     }
 };
 
-// DRY RUN 
-/*
- nums = [1,3,-1,-3,5] n = 5
-k = 3
-
-1 window 
-(3,1), (1,0), (-1,2)
-
-ans[0] = 3
-
-2 window  i = 3 = k  (1, 0) remove  push(-3 , 3)
-(3,1)(pq.top), (-1,2), (-3,3)
-
-ans[1] = 3
-
-Window 3 , i = 4  (3,1) remove push(5,4)
-(5,4), (-1,2), (-3,3)
-
-ans[2] = 5
-
-ans=[3 , 3 , 5] // no of elements = 5-3+1 = 3
-
-
-Time: O(n log n)
-
-Space: O(n) (heap storage)
-*/
-
+// tc-->o(N)  sc--> O(k)
