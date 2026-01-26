0e06eba3350e9217886757884b35e0f2601f3ca2
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-01-27 03:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (93.13%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,39 +11,27 @@
  */
 class Solution {
 public:
-    // using recursion
-    void preorderTraversal(TreeNode* root, vector<int> &arr){
-        // If the current node is NULL
-        // (base case for recursion), return
-        if(root == nullptr){
-            return;
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        vector<int> res ;
+        if (!root) return res;
+        stack<TreeNode*> st ;
+        st.push(root) ;
+
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+
+            res.push_back(node->val);
+
+            if(node->right) st.push(node->right);
+
+            if (node->left) {
+                st.push(node->left);
+            }
+            
         }
-        // Push the current node's
-        // value into the vector
-        arr.push_back(root->val);
-        // Recursively traverse
-        // the left subtree
-        preorderTraversal(root->left, arr);
-        // Recursively traverse 
-        // the right subtree
-        preorderTraversal(root->right, arr);
+        return res ;
+        
     }
-
-    // Function to initiate preorder traversal
-    // and return the resulting vector
-    vector<int> preorderTraversal(TreeNode* root){
-        // Create an empty vector to
-        // store preorder traversal values
-        vector<int> arr;
-        // Call the preorder traversal function
-        preorderTraversal(root, arr);
-        // Return the resulting vector
-        // containing preorder traversal values
-        return arr;
-    } 
-  
 };
-
-
-
-
