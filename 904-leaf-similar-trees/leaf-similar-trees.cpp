4e2a99b00e4c19da882cf95e7cc6445430f80b13
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-01-30 02:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (9.04%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -9,22 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ 
+//T.C : O(n)
+//S.C : O(n) (string s) - I am ignoring the recursion stack space here
 class Solution {
 public:
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> leaves1, leaves2;
-        getLeaves(root1, leaves1);
-        getLeaves(root2, leaves2);
-        return leaves1 == leaves2;
+    
+    void inOrder(TreeNode* root, string& s) {
+        if(root == NULL) return;
+        
+        //leaf node
+        if(root->left == NULL && root->right == NULL) {
+            s += to_string(root->val) + "_";
+            return;
+        }
+        
+        inOrder(root->left, s);
+        inOrder(root->right, s);
     }
-private:
-    // DFS
-    void getLeaves(TreeNode* node, vector<int>& leaves) {
-      if (!node) return;
-      if (!node->left && !node->right) {
-        leaves.push_back(node->val);
-      }
-      getLeaves(node->left, leaves);
-      getLeaves(node->right, leaves);
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        string s1 = "";
+        string s2 = "";
+        
+        inOrder(root1, s1);
+        inOrder(root2, s2);
+        
+        return s1==s2;
     }
 };
