1b48ea76d622b2f4b8cb92bd3be3be25a20678bd
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-01-30 02:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (46.14%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,29 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//Approach-2 (Using BFS)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
-        if((p == NULL && q == NULL)) return true ;
-        if(p==NULL || q==NULL){
-            return false ;
+        if(p == NULL && q == NULL)
+            return true;
+        if(p == NULL || q == NULL)
+            return false;
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(p);
+        q2.push(q);
+        while(!q1.empty() && !q2.empty()) {
+            TreeNode* first = q1.front();
+            TreeNode* second = q2.front();
+            q1.pop();
+            q2.pop();
+            if(first->val != second->val)
+                return false;
+          
+            if(first->left != NULL && second->left != NULL) {
+                q1.push(first->left);
+                q2.push(second->left);
+            } else if(first->left != NULL || second->left != NULL)
+                return false;
+          
+            if(first->right && second->right) {
+                q1.push(first->right);
+                q2.push(second->right);
+            } else if(first->right || second->right)
+                return false;
         }
-
-        if(p->val != q->val) return false ;
-        
-        bool left =  isSameTree(p->left , q->left) ;
-        bool right =  isSameTree(p->right , q->right) ;
-
-        return (left && right) ;
-        
+        return true; 
     }
 };
