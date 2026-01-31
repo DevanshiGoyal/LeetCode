9545800f0e50015513c385de8724634488cdcb69
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-01-31 19:26:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (24.67%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        // using binary search 
+        // linear search  // O(N)
+        for(int i = 0; i<letters.size(); i++){
+            if(letters[i]>target) return letters[i];
+        }
+        return letters[0];
+    }
+};
+
+/*
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        // Using binary search  // O(logN)
         int left = 0;
         int right = letters.size() - 1;
 
@@ -20,3 +32,4 @@ public:
         return letters[left];
     }
 };
+*/
