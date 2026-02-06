17b4f8e0ab9d0016fc662d10916cfb3fd8e34011
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-02-06 23:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.61%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,23 @@
-class Solution {
-public:
+
     // using rescursion ---> TLE
     // memoization ---> MLE
     // tabulation ---> TC--->O(N) SC--->O(N)
     // space optimization 
-    
+
+class Solution {
+public:
+    int solve(int n, vector<int>& memo) {
+        if (n < 0) return 0;
+       
+        if (n == 0) return 1;
+
+        if (memo[n] != -1) return memo[n];
+
+        return memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
+    }
 
     int climbStairs(int n) {
-    if (n == 0 || n == 1) {
-        return 1;
+        vector<int> memo(n + 1, -1);
+        return solve(n, memo);
     }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
-    }
-
-
-
 };
