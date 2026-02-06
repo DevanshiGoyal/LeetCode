dd7d86cc466ce0e9553d37da0e4985556708b1aa
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-02-06 18:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-// using memoization
-    int solve(int n, vector<int>& dp) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        // overlapping subproblems -->memoization  
-        if (dp[n] != -1) return dp[n];
-
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-    }
 
     int fib(int n) {
-        vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        if(n<=1) return n ;
+        vector<int> dp(n+1 , -1);
+
+        // dp[i] = ith fibonacci number 
+        dp[0] = 0 ;
+        dp[1] = 1 ;
+        
+        for(int i = 2 ; i<=n ; i++){
+            dp[i] = dp[i-1] + dp[i-2] ;
+        }
+
+        return dp[n] ;
     }
 };
 /*
 TC  ---> O(n)
-SC  ---> O(n)  (dp array + recursion stack)
+SC  ---> O(n)  (dp array only)
 */
