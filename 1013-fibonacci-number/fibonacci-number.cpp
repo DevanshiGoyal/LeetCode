f33f29768ce39c3d40cd3dccc2a69ea164495ab0
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-02-06 20:00:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (36.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
 
     int fib(int n) {
-        if(n<=1) return n ;
-        vector<int> dp(n+1 , -1);
-
-        // dp[i] = ith fibonacci number 
-        dp[0] = 0 ;
-        dp[1] = 1 ;
+        // space optimised
+        if(n<=1) return n ; 
+        int a = 0 ;
+        int b = 1 ;
+        int c;
+        for(int i = 1 ; i<n ; i++){
+            c = a + b ;
+            a=b ;
+            b=c ;
         
-        for(int i = 2 ; i<=n ; i++){
-            dp[i] = dp[i-1] + dp[i-2] ;
         }
 
-        return dp[n] ;
+        return c ;
     }
 };
 /*
 TC  ---> O(n)
-SC  ---> O(n)  (dp array only)
+SC  ---> O(1)  
 */
