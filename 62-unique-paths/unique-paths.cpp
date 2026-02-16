4477993dcf063af27b93d77a74015797f6bbf0db
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-02-17 02:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.12%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,90 +1,31 @@
 class Solution {
 public:
-    // using recursion
-    // TLE
-    // TC--> O(2^(m+n))
-    int recursion(int i , int j){
-        // base case 
-        if(i == 0  && j == 0) return 1 ;
-        if(i<0 || j<0) return 0 ;
+    vector<vector<int>> dp;
+    int memo(int i , int j, int m , int n){
+        // base cases
 
-        int up = recursion(i-1 , j);
-        int left = recursion(i , j-1) ;
+        // out of grid
+        if(i>=m && j>=n) return 0 ;
 
-        return up + left ;
-
-    }
-
-    int memoization(int i , int j  , vector<vector<int>> &dp ){
-
-        // TC : O(M * N)
-        // SC : O((M - 1) + (N - 1)) + O(M * N)
-
-        if(i == 0  && j == 0) return 1 ;
-        if(i<0 || j<0) return 0 ;
+        // reached destination
+        if(i==m-1 || j==n-1) return 1 ;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up = memoization(i-1 , j , dp );
-        int left = memoization(i , j-1 , dp ) ;
+        int down = memo(i+1 , j , m , n) ;
+        int right = memo(i , j+1 , m , n) ;
 
-        return dp[i][j] = up + left ;
-
-    }
-    // Tabulation 
-    // stack Optimisation
-    int tabulation(int m , int n , vector<vector<int>> &dp ){
-
-        // TC : O(M * N)
-        // SC : O(M * N)
-        
-        for(int  i = 0 ; i< m ; i++){
-            for(int j = 0 ; j< n ; j++){
-                if(i == 0  && j == 0) dp[i][j] = 1;
-                else{
-
-                    int up = 0, left = 0;
-                    if (i > 0) up = dp[i - 1][j];
-                    if (j > 0) left = dp[i][j - 1];
-                    dp[i][j] = up + left;
-
-                }
-            }
-        }
-        
-        return dp[m - 1][n - 1];
-
-    }
-
-    int tabulationSpaceOptimized(int m, int n) { 
-        // TC : O(M * N)
-        // SC : O(N)
-        vector<int> prev(n , 0); //Prev row of size number of columns
-        for (int i = 0; i < m; i++) {
-            vector<int> curr(n , 0); // current row 
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) curr[j] = 1;
-                else {
-                    int up = 0, left = 0;
-                    if (i > 0) up = prev[j];
-                    if (j > 0) left = curr[j - 1];
-                    curr[j] = up + left;
-                }
-            }
-            prev = curr;
-        }
-        return prev[n - 1];
-
+        return dp[i][j] = down + right ;
     }
 
     int uniquePaths(int m, int n) {
+        dp.resize(m , vector<int>(n,-1));
+        return memo(0 , 0 , m , n) ;
 
-        vector<vector<int>>dp(m , vector<int>(n , -1)) ;
-        //return recursion(m-1 , n-1) ;
-        
-        //return memoization(m - 1, n - 1, dp);
-        //return tabulation(m, n, dp);
-        return tabulationSpaceOptimized(m, n);
+        //return recursion(0 , 0 , m , n) ;
         
     }
 };
+
+// TC--> O(m*n)
+// SC--> O(m*n)
