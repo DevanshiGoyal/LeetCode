654f935884e1758aa491520cb59a26d7473ff9dd
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-19 16:38:43 +0530

    Time: 14 ms (29.52%) | Memory: 21.3 MB (32.14%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -3,25 +3,39 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         stack<int> st;
-        st.push(-1);
+        
 
         vector<int> prevSmaller(n), nextSmaller(n);
 
-        for (int i = 0; i < n; i++) {
-            while (st.top() != -1 && heights[st.top()] >= heights[i])
-                st.pop();
-            prevSmaller[i] = st.top();
-            st.push(i);
+        
+
+        for(int i = 0 ; i<n ; i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop() ;
+                
+            }
+            if(st.empty()){
+                prevSmaller[i] = -1 ;
+
+            }else prevSmaller[i] = st.top() ;
+
+            st.push(i) ;
         }
 
         while (!st.empty()) st.pop();
-        st.push(-1);
 
-        for (int i = n - 1; i >= 0; i--) {
-            while (st.top() != -1 && heights[st.top()] >= heights[i])
-                st.pop();
-            nextSmaller[i] = (st.top() == -1) ? n : st.top();
-            st.push(i);
+        //NSR
+
+        for (int i = n - 1; i >= 0; i--){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop() ;
+                
+            }
+            if(st.empty()){
+                nextSmaller[i] = n ;
+            }else nextSmaller[i] = st.top() ;
+
+            st.push(i) ;
         }
 
         int maxArea = 0;
