8c6d4a58dc1ab631fde3c4ce43da6bc59fe2db0d
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-21 00:30:11 +0530

    Time: 407 ms (5.15%) | Memory: 17.8 MB (59.31%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        //optimised appproach
-        // based on observation 
-        // all positive ---> then take all array 
-        // even negatives --> then also take whole array 
-        // odd negatives ---> remove one negative values ----> prefix and suufix prod 
-        // using prefix and suffix product
-        // TC--->O(N)  SC--->O(1)
+        // brute force approach 
+        // will take all subarrays 
+        // try to find prod and then update max prod 
 
-        int n = nums.size();
-        int pref = 1 , suff = 1 ;
-        int ans = INT_MIN ;
-
+        int n = nums.size() ;
+        int maxprod = INT_MIN ;
         for(int i = 0 ; i<n ; i++){
-            //Reset prefix and suffix to 1 if they become 0 (cases where product of  subarray becomes 0)
-            if(pref == 0) pref = 1;
-            if(suff == 0) suff = 1;
-
-            pref = pref*nums[i] ;
-            suff = suff * nums[n-i-1] ;
-
-            ans = max(ans , max(pref , suff));
+            int prod = 1 ;
+            for(int j = i ; j<n ; j++){
+                prod = prod*nums[j] ;
+                maxprod = max(prod , maxprod) ;
+                
+            }
 
         }
-        return ans ;
+
+        return maxprod ;
         
     }
 };
