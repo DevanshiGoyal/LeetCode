6624401145036577842063a08239167154208a78
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-21 00:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.64%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // brute force approach 
-        // will take all subarrays 
-        // try to find prod and then update max prod 
+        // better approacj approach 
+        /*
+        I initialize max and min with first element
+
+At each step, I decide whether to start fresh or extend
+
+If number is negative, roles of max and min flip
+
+Finally, I keep updating global answer
+        */
 
         int n = nums.size() ;
-        int maxprod = INT_MIN ;
-        for(int i = 0 ; i<n ; i++){
-            int prod = 1 ;
-            for(int j = i ; j<n ; j++){
-                prod = prod*nums[j] ;
-                maxprod = max(prod , maxprod) ;
-                
+        int maxProd = nums[0];   // max product till now
+        int minProd = nums[0];   // min product till now
+        int ans = nums[0];       // final answer
+        
+        for(int i = 1 ; i<n ; i++){
+            // If current element is negative ->swap
+            if(nums[i] < 0){
+                swap(maxProd , minProd) ;
             }
 
+            // either take current element OR extend subarray
+            maxProd = max(nums[i], maxProd * nums[i]);
+            minProd = min(nums[i], minProd * nums[i]);
+            
+
+            ans = max(ans, maxProd);
         }
 
-        return maxprod ;
+        return ans;
         
     }
 };
+/*
+tc-->O(n)
+sc-->o(n)
+*/
