34952d26fea8b44268cb89a4a4b191a083420f44
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-22 21:53:32 +0530

    Time: 26 ms (66.73%) | Memory: 27.4 MB (52.33%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    // shifting of index by 1 
-    // space optimization 
-    // tc--->O(n*m)
-    // sc--->O(m)
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
+    // using recursion -->TLE
+    //Memoization -->tle
+    // overlapping subproblems
+    // Bottom up approach 
+    // TC---> O((m*n))  SC-->O(m*n) 
+    
+    int longestCommonSubsequence(string s1, string s2) {
+       
+        int  m = s1.length() ;
+        int n = s2.length() ;
 
-        vector<int> prev(m + 1, 0), curr(m + 1, 0);
-
-        // base cases
-        for (int i = 0; i <= n; i++) {
-            prev[0] = 0;
+        vector<vector<int>>dp(m+1 , vector<int>(n+1)) ;
+        
+        for(int r = 0 ; r<m+1 ; r++){
+            dp[r][0] = 0 ;
+        }
+        for(int c = 0 ; c<n+1 ; c++){
+            dp[0][c] = 0 ;
         }
 
-        // fill in the DP table to calc the length of LCS
-        for (int ind1 = 1; ind1 <= n; ind1++) {
-            for (int ind2 = 1; ind2 <= m; ind2++) {
-                if (text1[ind1 - 1] == text2[ind2 - 1])
-                    curr[ind2] = 1 + prev[ind2 - 1]; // Characters match, increment LCS length
-                else
-                    curr[ind2] = max(prev[ind2], curr[ind2 - 1]); // Characters don't match
+        for(int i = 1 ; i<m+1 ; i++){
+            for(int j =1 ;j<n+1 ; j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = 1+ dp[i-1][j-1] ;
+                }
+                else{
+                    dp[i][j] = max(dp[i][j-1] , dp[i-1][j]) ;
+                }
+
             }
-            prev = curr ;
         }
-
-        return prev[m]; // Return the length of the Longest Common Subsequence
-            
+        return dp[m][n];
+        
     }
 };
