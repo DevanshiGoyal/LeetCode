feb428f923d5dc58f09fc3ccebb61ef0bb5aad38
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-22 22:49:58 +0530

    Time: 23 ms (82.41%) | Memory: 27.4 MB (68.17%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,35 +4,52 @@ public:
     //Memoization -->tle
     // overlapping subproblems
     // Bottom up approach 
-    // space optimised 1D optimised 
-    // TC---> O((m*n))  SC-->O(n) 
+    // TC---> O((m*n))  SC-->O(m*n) 
     
     int longestCommonSubsequence(string s1, string s2) {
        
-        int  n = s1.length() ;
-        int m = s2.length() ;
+        int  m = s1.length() ;
+        int n = s2.length() ;
 
-       vector<int> prev(m + 1, 0), curr(m + 1, 0);
-
-       // base cases
-        for (int i = 0; i <= n; i++) {
-            prev[0] = 0;
+        vector<vector<int>>dp(m+1 , vector<int>(n+1)) ;
+        
+        for(int r = 0 ; r<m+1 ; r++){
+            dp[r][0] = 0 ;
+        }
+        for(int c = 0 ; c<n+1 ; c++){
+            dp[0][c] = 0 ;
         }
 
-        for(int i = 1 ; i<=n ; i++){
-            for(int j =1 ;j<=m ; j++){
-                
+        for(int i = 1 ; i<m+1 ; i++){
+            for(int j =1 ;j<n+1 ; j++){
                 if(s1[i-1]==s2[j-1]){
-                    curr[j] = 1+ prev[j-1] ;
+                    dp[i][j] = 1+ dp[i-1][j-1] ;
                 }
                 else{
-                    curr[j] = max(curr[j-1] , prev[j]) ;
+                    dp[i][j] = max(dp[i][j-1] , dp[i-1][j]) ;
                 }
-                
+
             }
-            prev = curr ;
         }
-        return prev[m];
+        string lcs ="";
+        int i = m , j= n ;
+        while(i>0 && j>0){
+            if(s1[i-1]==s2[j-1]){
+                lcs.push_back(s1[i-1]);
+                i-- , j-- ;
+
+            }
+            else{
+                if(dp[i-1][j]> dp[i][j-1]){
+                    i--;
+                }else j--;
+            }
+        }
+        reverse(lcs.begin() , lcs.end());
+        cout<<lcs<<endl;
+        return dp[m][n];
+
+
         
     }
 };
