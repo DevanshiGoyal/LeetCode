71ecdaf820774c38145d7b8d49192c83fa24eb9b
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-22 16:06:46 +0530

    Time: 10 ms (36.09%) | Memory: 13.2 MB (59.10%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-//Time: O(m*n), Space: O(m*n);
+    //Time: O(m*n), Space: O(m*n);
     int minDistance(string word1, string word2) {
-
-        int m = word1.length();
-        int n = word2.length();
-
+        int m = word1.length(), n = word2.length();
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-
-        for(int i = 0 ; i<=m ; i++){
-            for(int j=0 ; j<=n ; j++){
-                if(i==0 || j==0){
-                    dp[i][j] = i+j;
-                }else if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1] ;
-                }
+        
+        //Base conditions
+        for(int i=0; i<m+1; i++)
+            dp[i][0] = i;
+        
+        for(int j=0; j<n+1; j++)
+            dp[0][j] = j;
+        
+        //solving the sub-problems
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
                 else{
-                    int insertchar = dp[i][j-1] ;
-                    int deletechar = dp[i-1][j] ;
-                    int replacechar = dp[i-1][j-1] ;
-
-                    dp[i][j] = 1+ min({insertchar , deletechar ,replacechar});
+                    int insertChar = dp[i][j-1];
+                    int deleteChar = dp[i-1][j];
+                    int replaceChar = dp[i-1][j-1];
+                    
+                    dp[i][j] = 1 + min({insertChar, deleteChar, replaceChar});
                 }
             }
         }
-
-        return dp[m][n] ;
-        
+        return dp[m][n];
     }
 };
