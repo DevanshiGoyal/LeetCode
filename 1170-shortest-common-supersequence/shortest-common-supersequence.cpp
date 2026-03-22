dc55189434397469f135e20e545e811a5f9390ac
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-03-23 01:00:06 +0530

    Time: 30 ms (12.53%) | Memory: 26.7 MB (70.62%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
+    //T.C : O(m*n)
+    //S.C : O(m*n)
     string shortestCommonSupersequence(string str1, string str2) {
         
         int m = str1.size(), n = str2.size();
-        // Create a DP table to store lengths of longest common subsequences.
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        //  DP table to store len of longest common subsequences
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
 
-        // Fill the DP table
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+        // fill DP table
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if( i==0 || j==0)  dp[i][j] = i+j;
+                else if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1] ;
                 } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        // Reconstruct the shortest common supersequence from the DP table
-        string scs;
+        // build the shortest common supersequence from  DP table
+        string scs="";
         int i = m, j = n;
         while (i > 0 && j > 0) {
             if (str1[i - 1] == str2[j - 1]) {
                 scs.push_back(str1[i - 1]);
                 i--;
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                scs.push_back(str1[i - 1]);
-                i--;
-            } else {
-                scs.push_back(str2[j - 1]);
-                j--;
+            } else{ 
+                if (dp[i - 1][j] < dp[i][j - 1]) { // moves toward shorter 
+                    scs.push_back(str1[i - 1]);
+                    i--;
+                } else {
+                    scs.push_back(str2[j - 1]);
+                    j--;
+                }
             }
         }
 
-        // Add remaining characters of str1 and str2
+        //add remaining characters from both str1 and str2
+
         while (i > 0) {
             scs.push_back(str1[i - 1]);
             i--;
@@ -44,7 +50,7 @@ public:
             j--;
         }
 
-        // Reverse the constructed string since we've built it from the end
+        // Reverse the  string since we built it from  end
         reverse(scs.begin(), scs.end());
         return scs;
     }
