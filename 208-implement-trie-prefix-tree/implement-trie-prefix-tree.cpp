906aaa41244f29607a21cba0275e56a154a8bb25
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-02 19:17:59 +0530

    Time: 16 ms (81.53%) | Memory: 51.4 MB (22.97%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,32 +1,32 @@
-struct Node {
-    Node* links[26] = {nullptr};
-    bool flag = false;
+struct Node{
+    Node* links[26] = {nullptr} ;
+    bool flag = false ;
 
-    bool containsKey(char ch) {
-        return links[ch - 'a'] != nullptr;
+    bool containsKey(char ch){
+        return links[ch-'a'] !=nullptr ;
     }
 
-    void put(char ch, Node* node) {
-        links[ch - 'a'] = node;
+    void put(char ch , Node* node){
+        links[ch-'a'] = node ;
     }
 
-    Node* get(char ch) {
-        return links[ch - 'a'];
+    Node* get(char ch){
+        return links[ch-'a'] ;
     }
 
-    void setEnd() {
-        flag = true;
+    void setEnd(){
+        flag = true ;
     }
 
-    bool isEnd() {
-        return flag;
+    bool isEnd(){
+        return flag ;
     }
 
-    ~Node() {
-        for (int i = 0; i < 26; i++) {
-            if (links[i] != nullptr) {
-                delete links[i];
-                links[i] = nullptr;
+    ~Node(){
+        for(int i = 0 ; i<26 ; i++){
+            if(links[i] != nullptr){
+                delete links[i] ;
+                links[i] = nullptr ;
             }
         }
     }
@@ -34,51 +34,62 @@ struct Node {
 
 class Trie {
 private:
-    Node* root;
+    Node* root ;
 
 public:
     Trie() {
-        root = new Node();
+        root = new Node() ;
+        
+    }
+    ~Trie(){
+        delete root ;
     }
 
-    ~Trie() {
-        delete root;
-    }
-
+    
     void insert(string word) {
-        Node* node = root;
-        for (char ch : word) {
-            if (!node->containsKey(ch)) {
-                node->put(ch, new Node());
+        Node* node = root ;
+        for(char ch : word){
+            if(!node->containsKey(ch)){
+                node->put(ch , new Node()) ;
             }
-            node = node->get(ch);
-        }
-        node->setEnd();
-    }
 
-    bool search(string word) {
-        Node* node = root;
-        for (char ch : word) {
-            if (!node->containsKey(ch)) {
-                return false;
-            }
-            node = node->get(ch);
+            node = node->get(ch) ;
         }
-        return node->isEnd();
+
+        node->setEnd() ;
+        
+    }
+    
+    bool search(string word) {
+
+        Node* node = root ;
+        for(char ch : word){
+            if(!node->containsKey(ch)){
+                return false ;
+            }
+
+            node = node->get(ch) ;
+        }
+
+        return node->isEnd() ;
+        
     }
     
     bool startsWith(string prefix) {
-        Node* node = root;
-        for (char ch : prefix) {
-            if (!node->containsKey(ch)) {
-                return false;
+        Node* node = root ;
+        for(char ch : prefix){
+            if(!node->containsKey(ch)){
+                return false ;
             }
-            node = node->get(ch);
+
+            node = node->get(ch) ;
         }
-        return true;
+        return true ;
         
     }
 };
+
+// tc-->o(N)
 
 /**
  * Your Trie object will be instantiated and called as such:
@@ -87,5 +98,3 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
-
- 
