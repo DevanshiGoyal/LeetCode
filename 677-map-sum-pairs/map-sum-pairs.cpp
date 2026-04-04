2411718defe32ca2f3c4b75029d188d9c9394c52
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-04 19:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (14.62%) - LeetSync

--- 677-map-sum-pairs/map-sum-pairs.cpp
+++ 677-map-sum-pairs/map-sum-pairs.cpp
@@ -1,32 +1,68 @@
-class MapSum {
-private: 
-    map<string , int> mp;
-public:
-    MapSum() {}
+class node{
+  public:
+    node* v[26];
+    int val=0;
     
-    void insert(string key, int val) {
-        mp[key] = val;
-        
+    bool ispresent(char c){
+        return this->v[c-'a']!=NULL;
     }
     
-    int sum(string prefix) {
-        int sum = 0;
-
-        for(auto i : mp){
-            if((i.first).starts_with(prefix)){ //key.starts_with(prefix)
-                sum+=i.second;  // sum+=val
-            }
-        }
-
-        return sum ;
-//tc-->O(n*p)  sc-->O(n)
-        
+    void create(char c,node* n){
+        this->v[c-'a']=n;
+    }
+    
+    void addval(int value){
+        this->val+=value;
     }
 };
 
-/**
- * Your MapSum object will be instantiated and called as such:
- * MapSum* obj = new MapSum();
- * obj->insert(key,val);
- * int param_2 = obj->sum(prefix);
- */
+class trie{
+    public:
+    node* root=new node();
+    
+    void insert(string key,int val){
+        node* r1=root;
+        for(auto ele:key){
+            
+            if(!r1->ispresent(ele)){
+                r1->create(ele,new node());    
+            }
+            
+            r1=r1->v[ele-'a'];
+            r1->addval(val);
+        }
+    }
+    
+    int countprefixsum(string prefix){
+        node* r1=root;
+        
+        for(auto ele:prefix){
+            if(!r1->ispresent(ele))return 0;
+            r1=r1->v[ele-'a'];
+        }
+        return r1->val;
+    }
+};
+
+class MapSum {
+public:
+    
+    unordered_map<string,int>m;
+    trie* root;
+    MapSum() {
+        root=new trie();
+    }
+    
+    void insert(string key, int val) {
+        int a=val;
+        if(m.find(key)!=m.end()){
+            val-=m[key];
+        }    
+        m[key]=a;
+        root->insert(key,val);
+    }
+    
+    int sum(string prefix) {
+        return root->countprefixsum(prefix);
+    }
+};
