511fbd429b47762d7fe70241d94e117afd91951b
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-07 21:56:53 +0530

    Time: 4 ms (44.86%) | Memory: 18.5 MB (47.66%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* tail = dummy;
+        if (!l1) return l2;
+        if (!l2) return l1;
 
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                tail->next = l1;
-                l1 = l1->next;
-            } else {
-                tail->next = l2;
-                l2 = l2->next;
-            }
-            tail = tail->next;
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
-
-        if (l1) tail->next = l1;
-        if (l2) tail->next = l2;
-
-        return dummy->next;
     }
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty()) return nullptr;
+        return divideAndConquer(lists, 0, lists.size() - 1);
+    }
 
-        int n = lists.size();
+    ListNode* divideAndConquer(vector<ListNode*>& lists, int left, int right) {
+        if (left == right) return lists[left];
 
-        while (n > 1) {
-            int k = (n + 1) / 2;
-
-            for (int i = 0; i < n / 2; i++) {
-                lists[i] = mergeTwoLists(lists[i], lists[i + k]);
-            }
-
-            n = k;
-        }
-
-        return lists[0];
+        int mid = left + (right - left) / 2;
+        ListNode* l1 = divideAndConquer(lists, left, mid);
+        ListNode* l2 = divideAndConquer(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
     }
 };
