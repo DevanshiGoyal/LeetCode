068c26e4f6621ada3dd9457d6d5a98faa3d112ac
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-08 23:52:34 +0530

    Time: 3 ms (50.16%) | Memory: 18 MB (38.78%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int , int> mp ;
+        int n = nums.size() ;
 
-        for(int  i = 0 ; i<nums.size() ; i++){
+        unordered_map<int , int> mp;
+
+        for(int i = 0 ;i<n ; i++){
             mp[nums[i]]++;
         }
 
-        priority_queue<pair<int , int>> pq ;
+        priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-        for(auto e : mp){
-            pq.push({e.second , e.first}) ;
+        for(auto it: mp){
+            pq.push({it.second, it.first}); // freq, element
+            if(pq.size()>k) pq.pop(); 
         }
 
-        vector<int> ans ;
-
-        while(k-- && !pq.empty()){
-            ans.push_back(pq.top().second) ;
-            pq.pop() ;
-
-
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-        return ans ;
-
-
+        return ans;
         
     }
 };
+
+/*
+TC
+Freq map O(n) + Heap operations O(n log k)
+--> O(n log k)
+
+SC
+Hash map O(n) + Heap O(k)
+
+--> O(n)
+*/
