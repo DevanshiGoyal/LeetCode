78daf8caf333857a92164d3a0cae963498831715
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-14 04:57:31 +0530

    Time: 68 ms (28.99%) | Memory: 145.7 MB (9.46%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &graph,int currNode,vector<bool> &vis,int &nodeCount,int &edgeCount) {
-        vis[currNode] = true;
-        nodeCount += 1;
-        edgeCount += graph[currNode].size();
-        for(int nextNode : graph[currNode]) {
-            if(!vis[nextNode]) {
-                dfs(graph,nextNode,vis,nodeCount,edgeCount);
+    void bfs(int node,vector<vector<int>> &adj,vector<int> &comp,vector<bool> &vis){
+        queue<int> q;
+        q.push(node);
+        vis[node]=true;
+
+        while(!q.empty()){
+            int n=q.front();
+            q.pop();
+            comp.push_back(n);
+            for(auto it:adj[n]){
+                if(!vis[it]){
+                    q.push(it);
+                    vis[it]=true;
+                }
             }
         }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> graph(n);
-        for(vector<int> &e : edges) {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+        vector<vector<int>> adj(n);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        int ans = 0;
-        vector<bool> vis(n);
-        for(int i = 0; i < n; i += 1) {
-            int nodeCount = 0,edgeCount = 0;
-            if(!vis[i]) {
-                dfs(graph,i,vis,nodeCount,edgeCount);
-                if(nodeCount*(nodeCount - 1) == edgeCount) ans += 1;
+        int ans=0;
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;i++){
+
+            if(!vis[i]){
+                vector<int> comp;
+                bfs(i,adj,comp,vis);
+                bool iscomp=true;
+                for(auto it: comp){
+                    if(adj[it].size()!=comp.size()-1){
+                        iscomp=false;
+                        break;
+                    }
+                }
+                if(iscomp)ans++;
             }
         }
         return ans;
