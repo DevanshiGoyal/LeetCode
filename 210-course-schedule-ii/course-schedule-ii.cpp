a4b5a5b9a6a6d1c6cffe44847ab06173cc929cf0
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-29 04:46:41 +0530

    Time: 10 ms (14.42%) | Memory: 18.8 MB (34.50%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+        // using kahns algorithm 
+        vector<vector<int>>adj(numCourses);
+        vector<int> ans ;
+        vector<int> indegree(numCourses , 0) ;
+
+        for(auto courses : prerequisites){
+            adj[courses[1]].push_back(courses[0]) ;
+            
+        }
+
+        for(int i=0 ; i<numCourses ; i++){
+            for(auto&it : adj[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q ;
+
+        for(int i = 0 ; i<numCourses ; i++){
+            if(indegree[i] == 0) q.push(i) ;
+        }
+
+        while(!q.empty()){
+            int node = q.front() ;
+            q.pop() ;
+            ans.push_back(node) ;
+
+            for(auto it : adj[node]){
+                indegree[it] --  ;
+                if(indegree[it] == 0) q.push(it) ;
+            }
+        }
+
+        //it means loop does not exist
+        //if loop exist then there are less elements in ans vector  
+        if (ans.size()==numCourses) return ans;
+        else return {} ;
+        
+        
+    }
+};
+
+//Time Complexity: O(V+E)-->  Visit each course and its prerequisites once.
+// Space Complexity: O(V+E) Adjacency list + visited arrays
