efcc289bf6dcad5085ec0008c30c6d566db07b7c
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-30 03:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (55.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,67 +1,42 @@
 class Solution {
 public:
-    // using bfs
-    void bfs(unordered_map<int , vector<int>> &adj, int u , vector<bool> &visited ){
 
-        queue<int> q ;
-        q.push(u) ;
-        visited[u] = true ;
-
-        while(!q.empty()){
-            int u = q.front() ;
-            q.pop() ;
-
-            for(int &v : adj[u]){
-                if(!visited[v]){
-                    visited[v] = true ;
-                    q.push(v) ;
-                }
-            }
-        }
-
-        
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size() ;
-        //int c = isConnected[0].size() ;
 
-        // build graph 
-        unordered_map<int , vector<int>> adj ;
-        // adjacency list
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<n ;j++){
-                if(isConnected[i][j] == 1 && i!=j){
-                    adj[i].push_back(j) ;  // isConnected[i][j] == isConnected[j][i]  constraint given
-                    //adj[j].push_back(i) ;
+        int n = isConnected.size();
+
+        vector<bool> visited(n, false);
+
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i]) {
+
+                provinces++;
+
+                queue<int> q;
+                q.push(i);
+
+                visited[i] = true;
+
+                while (!q.empty()) {
+
+                    int u = q.front();
+                    q.pop();
+
+                    for (int v = 0; v < n; v++) {
+
+                        if (isConnected[u][v] == 1 && !visited[v]) {
+
+                            visited[v] = true;
+                            q.push(v);
+                        }
+                    }
                 }
             }
-
         }
 
-        vector<bool> visited(n , false);
-        int province = 0 ;
-        for(int u = 0 ; u<n ; u++){
-            if(!visited[u]){
-                province++;
-                bfs(adj , u ,visited) ;
-            }
-        }
-        return province ;
-        
+        return provinces;
     }
 };
-
-/*
-
-Tc
-Building graph: O(V2)
-DFS traversal: O(V+E)
-tc-> O(V2)
-
-sc
-
-adj list -> O(V+E)
-visited = O(V)
-queue -> O(V)
-sc->  O(V+E)
-*/
