de209673dbaa5ec2b12a1ae5b1b5b85a1c2bd91b
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-04-30 03:12:15 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (40.64%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,42 +1,55 @@
 class Solution {
 public:
-    void dfs(int node , vector<int> &vis , vector<vector<int>>& adjList){
-        vis[node] = 1 ;
-        for(auto &it : adjList[node]){
-            if(!vis[it]){
-                dfs(it , vis , adjList) ;
+    void dfs(unordered_map<int , vector<int>> &adj, int u , vector<bool> &visited ){
+        visited[u] = true ;
+
+        for(int &v : adj[u]){
+            if(!visited[v]){
+                dfs(adj , v , visited) ;
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size() ;  // rows
-        int m = isConnected[0].size() ; // cols 
+        int n = isConnected.size() ;
+        //int c = isConnected[0].size() ;
 
-        // adlist
-        vector<vector<int>> adjList(n) ;
-
-        // visited array
-        vector<int> visited(n, 0) ;
+        // build graph 
+        unordered_map<int , vector<int>> adj ;
+        // adjacency list
         for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);  // neighbours
+            for(int j = 0 ; j<n ;j++){
+                if(isConnected[i][j] == 1 && i!=j){
+                    adj[i].push_back(j) ;  // isConnected[i][j] == isConnected[j][i]  constraint given
+                    //adj[j].push_back(i) ;
                 }
-
             }
+
         }
 
-        int count = 0 ;
-        for(int i = 0 ;i<n ; i++){
-            if(!visited[i]){  // agr not visted by any chance then different comp. not connected 
-                count++;
-                dfs(i , visited , adjList) ; // for other neigh
+        vector<bool> visited(n , false);
+        int province = 0 ;
+        for(int u = 0 ; u<n ; u++){
+            if(!visited[u]){
+                province++;
+                dfs(adj , u ,visited) ;
             }
         }
-
-        return count ;
+        return province ;
         
     }
 };
-//tc-->O(n^2)   sc-->O(n)
+
+/*
+
+Tc
+Building graph: O(V2)
+DFS traversal: O(V+E)
+tc-> O(V2)
+
+sc
+
+adj list -> O(V+E)
+visited = O(V)
+recursion stack -> O(V)
+sc->  O(V+E)
+*/
