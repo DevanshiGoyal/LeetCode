387b2b82c76ea2301776ed053466d4bb3a051826
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-03 02:29:31 +0530

    Time: 3 ms (98.38%) | Memory: 11.5 MB (22.09%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,15 +9,22 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-       unordered_map< ListNode* , int >mpp;
-       ListNode* temp = head;
-       while(temp != nullptr){
-        if(mpp.find(temp) != mpp.end()){
-            return temp;
+        // Pointer's intialize at head of linkedlist
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast != nullptr && fast->next != nullptr){ // running the loop until 
+            slow = slow->next; // moving slow by 1
+            fast = fast->next->next; // moving fast by 2
+            if(slow == fast){ // if they meet
+                slow = head; // reset slow to head
+                while(slow != fast){ // run loop until again fast & slow don't collab
+                    slow = slow->next; // moving slow by 1
+                    fast = fast->next; // moving fast by 1 as well
+                }
+                return slow; // Or return fast same thing, they will return the tail where cycle starts
+            }
         }
-        mpp[temp] = 1;
-        temp = temp -> next;
+        return nullptr; // if there is no cycle, it will return null
        }
-       return nullptr;
-    }
 };
