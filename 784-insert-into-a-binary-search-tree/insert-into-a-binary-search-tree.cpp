1af7c22f6e1cc175c9c5d8a4b9aa59e833a2cfdc
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-07 02:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 59.4 MB (23.33%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -12,26 +12,27 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root==NULL) return new TreeNode(val) ;
-        TreeNode* curr = root ;
-        while(curr!=nullptr){
-            if(val>= curr->val){
-                if(curr->right!=NULL) curr= curr->right ;
-                else{
-                    curr->right = new TreeNode(val);
-                    break ;
+        if(root == nullptr) return new TreeNode(val);
+        
+        TreeNode* curr = root;
+        
+        while(true){ // running an infinity loop, look for the place for new node to add
+            if(curr->val < val){
+                if(curr->right != nullptr) curr = curr->right; // update current on right
+                else {
+                    curr->right = new TreeNode(val); // otherwise add current of right to new value TreeNode
+                    break; // breaking this infinity loop
                 }
-            }else{
-                if(curr->left!=NULL) curr= curr->left ;
+            }
+            else{
+                if(curr->left != nullptr) curr = curr->left; // update current on left
                 else{
-                    curr->left = new TreeNode(val);
-                    break ;
-
+                    curr->left = new TreeNode(val); // otherwise add current of left to new value TreeNode
+                    break; // breaking this infinity loop
                 }
             }
         }
-
-        return root ;
+        return root;
         
     }
 };
