22eb29dc3e62570f1dc0ca5d65431b9158122dd3
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-07 16:30:17 +0530

    Time: 188 ms (66.95%) | Memory: 279 MB (84.76%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -6,61 +6,41 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        
-        // to store created nodes
+    TreeNode* createBinaryTree(vector<vector<int>>& des) {
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> children;
 
-        unordered_map<int , TreeNode*> mp ;
-        // hashmap to track parent 
-        unordered_map<int , int> root ;
-        // 0 node not processed  // 1 node is currently root  // -1 has a parent 
-
-        for(auto& it : descriptions){
-            int parent = it[0] ;
-            int child = it[1] ;
-            int isLeft = it[2] ;
-
-            if(mp[parent] == 0){
-                TreeNode* temp = new TreeNode(parent);
-                mp[parent] = temp ;
-
+        for (auto &d : des) {
+            int p = d[0];
+            int c = d[1];
+            int isLeft = d[2];
+            if (mp.find(p) == mp.end()) {
+                mp[p] = new TreeNode(p);
             }
-
-            if(mp[child] == 0){
-                TreeNode* temp = new TreeNode(child);
-                mp[child] = temp ;
-
+            if (mp.find(c) == mp.end()) {
+                mp[c] = new TreeNode(c);
             }
-
-            if(isLeft == 1){
-                mp[parent]->left = mp[child] ;
-            }
-            else{
-                mp[parent]->right = mp[child] ;
-            }
-
-            if(root[parent] != -1){  // parent nhi h 
-                root[parent] = 1 ;   // parent bana diya 
-            }
-
-            root[child] = -1 ;  // is node ka parent h 
+            if (isLeft == 1)
+                mp[p]->left = mp[c];
+            else
+                mp[p]->right = mp[c];
+            children.insert(c);
         }
 
-        int value = 0;
-
-        for (auto &it : root) {
-            if (it.second == 1) {
-                value = it.first;
-                break;
+        for (auto &it : mp) {
+            if (children.find(it.first) == children.end()) {
+                return it.second;
             }
         }
 
-        return mp[value];  // vo rrot node return kr dega 
-        
+        return nullptr; 
     }
 };
+
+//tc-->O(n)  sc-->O(n)
