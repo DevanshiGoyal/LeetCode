038c76c94f67ea06b4503440124e5cae08af3289
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-08 16:30:13 +0530

    Time: 7 ms (62.22%) | Memory: 127.7 MB (78.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,56 +1,17 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int numsLessThanPivot = 0, numsEqualToPivot = 0, n = nums.size();
-        for (auto val: nums) {
-            if (val < pivot) numsLessThanPivot++;
-            else if (val == pivot) numsEqualToPivot++;
-        }
-        
-        int initialIndex = 0, equalIndex = numsLessThanPivot, finalIndex = numsLessThanPivot + numsEqualToPivot;
-        vector<int> ans(n);
-        
-        for (auto val: nums) {
-            if (val < pivot) ans[initialIndex++] = val;
-            else if (val == pivot) ans[equalIndex++] = val;
-            else ans[finalIndex++] = val;
-        }
-        return ans;
-    }
-};
-
-// tc-->O(n)  sc-->O(n)
-
-/*'
-class Solution {
-public:
-    vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
+        int initialIndex = 0, lastIndex = n - 1;
         vector<int> ans(n);
         
-        int left = 0;
-        int right = n - 1;
-        int i=0,j=n-1;
-        
-        while(i<n){
-            if (nums[i] < pivot) {
-                ans[left++] = nums[i];
-            }
-            
-            if (nums[j] > pivot) {
-                ans[right--] = nums[j];
-            }
-
-            i ++;
-            j --;
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            if (nums[i] < pivot) ans[initialIndex++] = nums[i];
+            if (nums[j] > pivot) ans[lastIndex--] = nums[j];
         }
         
-        //remaining indices will be filled with pivot
-        while (left <= right) {
-            ans[left++] = pivot;
-        }
+        while (initialIndex <= lastIndex) ans[initialIndex++] = pivot;
         
         return ans;
     }
 };
-*/
