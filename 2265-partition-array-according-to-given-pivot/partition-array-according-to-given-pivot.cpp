4c2e1034f8f23b156b401b5fc78dcaf3461c231e
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-08 16:12:34 +0530

    Time: 3 ms (87.42%) | Memory: 127.7 MB (78.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,46 +1,56 @@
-class Solution 
-{
+class Solution {
 public:
-    vector<int> pivotArray(vector<int>& nums, int pivot) 
-    {
-        vector<int> before, after;
-        int pivotCount = 0, j = 0;
-
-        
-        for (int num : nums) 
-        {
-            if (num < pivot)
-            {
-                before.push_back(num);
-            } 
-            else if (num > pivot)
-            {
-                after.push_back(num);
-            } 
-            else
-            {
-                pivotCount++;
-            } 
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int numsLessThanPivot = 0, numsEqualToPivot = 0, n = nums.size();
+        for (auto val: nums) {
+            if (val < pivot) numsLessThanPivot++;
+            else if (val == pivot) numsEqualToPivot++;
         }
-
-       
-        for (int num : before)
-        {
-            nums[j++] = num;
-        } 
         
-        while (pivotCount--)
-        {
-            nums[j++] = pivot;
-        } 
+        int initialIndex = 0, equalIndex = numsLessThanPivot, finalIndex = numsLessThanPivot + numsEqualToPivot;
+        vector<int> ans(n);
         
-        for (int num : after)
-        {
-            nums[j++] = num;
-        } 
-
-        return nums;
+        for (auto val: nums) {
+            if (val < pivot) ans[initialIndex++] = val;
+            else if (val == pivot) ans[equalIndex++] = val;
+            else ans[finalIndex++] = val;
+        }
+        return ans;
     }
 };
 
 // tc-->O(n)  sc-->O(n)
+
+/*'
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+        vector<int> ans(n);
+        
+        int left = 0;
+        int right = n - 1;
+        int i=0,j=n-1;
+        
+        while(i<n){
+            if (nums[i] < pivot) {
+                ans[left++] = nums[i];
+            }
+            
+            if (nums[j] > pivot) {
+                ans[right--] = nums[j];
+            }
+
+            i ++;
+            j --;
+        }
+        
+        //remaining indices will be filled with pivot
+        while (left <= right) {
+            ans[left++] = pivot;
+        }
+        
+        return ans;
+    }
+};
+*/
