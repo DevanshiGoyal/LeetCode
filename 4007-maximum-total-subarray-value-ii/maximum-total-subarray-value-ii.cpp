76dfdf95017740e2d93f46ee0915edc5210acc66
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-11 00:51:25 +0530

    Time: 270 ms (92.74%) | Memory: 159.7 MB (77.42%) - LeetSync

--- 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
+++ 4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
@@ -1,42 +1,202 @@
 class Solution {
-    int size;
-    vector<int> seg_max, seg_min;
+public:
 
-    pair<int, int> query(int l, int r) {
-        int mx = 0, mn = 2e9;
-        for (l += size, r += size; l <= r; l /= 2, r /= 2) {
-            if (l % 2 == 1) { mx = max(mx, seg_max[l]); mn = min(mn, seg_min[l]); l++; }
-            if (r % 2 == 0) { mx = max(mx, seg_max[r]); mn = min(mn, seg_min[r]); r--; }
+    typedef long long ll;
+
+    int n;
+
+    vector<int> getnextSmallerLeft(vector<int>& nums){
+
+        stack<int> st;
+        vector<int> NSL(n,-1);
+
+        for(int i=0;i<n;i++){
+
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                NSL[i] = st.top();
+            }
+
+            st.push(i);
         }
-        return {mx, mn};
+
+        return NSL;
     }
 
-public:
+    vector<int> getnextSmallerRight(vector<int>& nums){
+
+        stack<int> st;
+        vector<int> NSR(n,n);
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                NSR[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        return NSR;
+    }
+
+    vector<int> getnextGreaterLeft(vector<int>& nums){
+
+        stack<int> st;
+        vector<int> NGL(n,-1);
+
+        for(int i=0;i<n;i++){
+
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                NGL[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        return NGL;
+    }
+
+    vector<int> getnextGreaterRight(vector<int>& nums){
+
+        stack<int> st;
+        vector<int> NGR(n,n);
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                NGR[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        return NGR;
+    }
+
     long long maxTotalValue(vector<int>& nums, int k) {
-        int n = nums.size();
-        size = 1; while (size < n) size *= 2;
-        seg_max.assign(2 * size, 0);
-        seg_min.assign(2 * size, 2e9);
-        for (int i = 0; i < n; i++) { seg_max[size + i] = seg_min[size + i] = nums[i]; }
-        for (int i = size - 1; i > 0; i--) {
-            seg_max[i] = max(seg_max[2 * i], seg_max[2 * i + 1]);
-            seg_min[i] = min(seg_min[2 * i], seg_min[2 * i + 1]);
+
+        n = nums.size();
+
+        vector<int> nextSmallerLeft = getnextSmallerLeft(nums);
+        vector<int> nextSmallerRight = getnextSmallerRight(nums);
+
+        vector<int> nextGreaterLeft = getnextGreaterLeft(nums);
+        vector<int> nextGreaterRight = getnextGreaterRight(nums);
+
+        vector<int> ord(n);
+
+        for(int i=0;i<n;i++){
+            ord[i] = i;
         }
-        priority_queue<pair<int, pair<int, int>>> heap;
-        for (int l = 0; l < n; l++) {
-            auto res = query(l, n - 1);
-            heap.push({res.first - res.second, {l, n - 1}});
+
+        sort(ord.begin(),ord.end(),
+        [&](int a,int b){
+
+            if(nums[a] != nums[b]){
+                return nums[a] < nums[b];
+            }
+
+            return a < b;
+        });
+
+        priority_queue<
+            pair<ll,pair<int,int>>
+        > maxHeap;
+
+        for(int hi=0;hi<n;hi++){
+
+            ll diff =
+            (ll)nums[ord[hi]]
+            - nums[ord[0]];
+
+            maxHeap.push({
+                diff,
+                {hi,0}
+            });
         }
-        long long ans = 0;
-        while (k-- && !heap.empty()) {
-            auto top = heap.top(); heap.pop();
-            ans += top.first;
-            int l = top.second.first, r = top.second.second;
-            if (r > l) {
-                auto res = query(l, r - 1);
-                heap.push({res.first - res.second, {l, r - 1}});
+
+        ll totalVal = 0;
+        ll remaining = k;
+
+        while(remaining > 0 && !maxHeap.empty()){
+
+            ll score = maxHeap.top().first;
+
+            int hi = maxHeap.top().second.first;
+            int lo = maxHeap.top().second.second;
+
+            maxHeap.pop();
+
+            int maxValIdx = ord[hi];
+            int minValIdx = ord[lo];
+
+            int leftBoundary =
+            max(
+                nextGreaterLeft[maxValIdx],
+                nextSmallerLeft[minValIdx]
+            ) + 1;
+
+            int rightBoundary =
+            min(
+                nextGreaterRight[maxValIdx],
+                nextSmallerRight[minValIdx]
+            ) - 1;
+
+            int leftIdx =
+            min(maxValIdx,minValIdx);
+
+            int rightIdx =
+            max(maxValIdx,minValIdx);
+
+            if(leftBoundary <= leftIdx &&
+               rightBoundary >= rightIdx){
+
+                ll leftChoices =
+                leftIdx - leftBoundary + 1;
+
+                ll rightChoices =
+                rightBoundary - rightIdx + 1;
+
+                ll totalSubarrays =
+                leftChoices * rightChoices;
+
+                ll take =
+                min(remaining,totalSubarrays);
+
+                totalVal += take * score;
+
+                remaining -= take;
+            }
+
+            if(lo + 1 <= hi){
+
+                ll newScore =
+                (ll)nums[ord[hi]]
+                - nums[ord[lo+1]];
+
+                maxHeap.push({
+                    newScore,
+                    {hi,lo+1}
+                });
             }
         }
-        return ans;
+
+        return totalVal;
     }
 };
