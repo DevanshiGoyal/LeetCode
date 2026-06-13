66a89c6464b7966e31dbab03302aba83d5cc3024
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-14 01:28:15 +0530

    Time: 3 ms (81.31%) | Memory: 14.4 MB (79.70%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    int solve(int ind, int buy, int cap,vector<int>& prices,vector<vector<vector<int>>>& dp) {
-
-        if (ind == prices.size() || cap == 0)
-            return 0;
-
-        if (dp[ind][buy][cap] != -1)
-            return dp[ind][buy][cap];
-
-        if (buy) {
-            return dp[ind][buy][cap] = max(solve(ind + 1, 1, cap, prices, dp),
-                -prices[ind] + solve(ind + 1, 0, cap, prices, dp)
-            );
-        }
-        else {
-            return dp[ind][buy][cap] = max(solve(ind + 1, 0, cap, prices, dp),
-                prices[ind] + solve(ind + 1, 1, cap - 1, prices, dp)
-            );
-        }
-    }
-
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2, vector<int>(k + 1, -1))
-        );
+        vector<vector<int>> ahead(2, vector<int>(k + 1, 0));
+        vector<vector<int>> curr(2, vector<int>(k + 1, 0));
 
-        return solve(0, 1, k, prices, dp);
+        for (int ind = n - 1; ind >= 0; ind--) {
+
+            for (int buy = 0; buy <= 1; buy++) {
+
+                for (int cap = 1; cap <= k; cap++) {
+
+                    if (buy) {
+                        curr[buy][cap] =
+                            max(ahead[1][cap],
+                                -prices[ind] + ahead[0][cap]);
+                    }
+                    else {
+                        curr[buy][cap] =
+                            max(ahead[0][cap],
+                                prices[ind] + ahead[1][cap - 1]);
+                    }
+                }
+            }
+
+            ahead = curr;
+        }
+
+        return curr[1][k];
     }
 };
 /*
-Time Complexity  O(N × 2 × K)
+Time Complexity  O(NK)
 
-Space Complexity  DP array: O(N × 2 × K) + O(N) (recursion stack)
+Space Complexity O(K)
 */
