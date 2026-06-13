9d119b7f917b523546a27bb31e1bfa1d41d3dae5
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-14 01:54:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (49.66%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    int solve(int ind, int buy,
-              vector<int>& prices,
-              vector<vector<int>>& dp) {
-
-        if (ind >= prices.size())
-            return 0;
-
-        if (dp[ind][buy] != -1)
-            return dp[ind][buy];
-
-        if (buy) {
-            return dp[ind][buy] = max(
-                solve(ind + 1, 1, prices, dp),
-                -prices[ind] + solve(ind + 1, 0, prices, dp)
-            );
-        } else {
-            return dp[ind][buy] = max(
-                solve(ind + 1, 0, prices, dp),
-                prices[ind] + solve(ind + 2, 1, prices, dp)
-            );
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
 
-        return solve(0, 1, prices, dp);
+        for (int ind = n - 1; ind >= 0; ind--) {
+
+            dp[ind][1] = max(
+                dp[ind + 1][1],
+                -prices[ind] + dp[ind + 1][0]
+            );
+
+            dp[ind][0] = max(
+                dp[ind + 1][0],
+                prices[ind] + dp[ind + 2][1]
+            );
+        }
+
+        return dp[0][1];
     }
 };
 
 /*
 Time Complexity: O(N * 2)
-Reason: There are N*2 states and each state is solved once.
+Reason: We iterate through N indices and 2 buy states.
 
-Space Complexity: O(N * 2) + O(N)
-Reason:
-1. DP array uses O(N*2).
-2. Recursion stack uses O(N).
+Space Complexity: O(N * 2)
+Reason: DP table of size (N+2) x 2.
 */
