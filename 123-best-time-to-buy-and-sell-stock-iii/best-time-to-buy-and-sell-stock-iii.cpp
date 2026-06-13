3ab32fc743bb11760c74db88544f7666b920c13c
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-14 01:06:51 +0530

    Time: 481 ms (8.25%) | Memory: 223.8 MB (27.18%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int buy1 = INT_MIN, sell1 = 0, buy2 = INT_MIN, sell2 = 0;
-        for(int i = 0, n = prices.size(); i < n; ++i){
-            const int p = prices[i];
-            buy1 = max(buy1 , -p);
-            sell1 = max(sell1 , p + buy1);
-            buy2 = max(buy2 , sell1 - p);
-            sell2 = max(sell2 , p + buy2);
+    int recursion(int ind , int buy , int cap, vector<int>& prices, vector<vector<vector<int>>>& memo){
+        int n = prices.size();
+        // base cases
+        if(cap==0) return 0 ;
+        if(ind == n) return 0 ;
+        if(memo[ind][buy][cap] != -1) return  memo[ind][buy][cap];
+        int profit = 0;
+        if(buy){
+            profit =  max(-prices[ind] + recursion(ind+1 , 0 , cap , prices, memo) , 0 + recursion(ind+1 , 1 , cap,prices , memo ));
+        }else{
+            profit =  max(prices[ind] + recursion(ind+1 , 1 , cap-1 , prices, memo) , 0 + recursion(ind+1 , 0 , cap, prices, memo ));
         }
-        return sell2;
+        return memo[ind][buy][cap] = profit ;
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> memo(n , vector<vector<int>>(2, vector<int>(3,-1)));  //size [n][2][3]
+        return recursion(0 , 1 , 2 , prices , memo);
+        
     }
 };
+/*
+Time Complexity: O(N*2*3) , There are N*2*3 states therefore at max ‘N*2*3’ new problems will be solved.
+
+Space Complexity: O(N*2*3) + O(N) , We are using a recursion stack space(O(N)) and a 3D array ( O(N*2*3)).
+*/
