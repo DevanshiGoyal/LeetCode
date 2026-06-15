4c55389b2ca5724c2471484cc7334c9bfb057b67
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-15 20:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (88.60%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st ;
+
+        for(char c : s){
+            if(c=='(' || c =='{' || c=='['){
+                st.push(c) ;
+            }
+            else{
+                if(!st.empty()){
+                    char top = st.top() ;
+                    if(c==')' && top =='(' || c=='}' && top =='{' || c==']' && top =='[' ){
+                        st.pop();
+                        
+                    }else{
+                        return false ;
+                    }
+                }else{
+                    return false ;
+
+                }
+   
+
+            }
+        }
+        if(st.empty()){
+            return true ;
+
+        }else{
+            return false ;
+        }
+            
+            
+    }
+};
