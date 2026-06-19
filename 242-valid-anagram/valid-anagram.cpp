a817c4b183ba5cd0b5800cc7bbce328899571978
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-20 02:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (92.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
+        // using hashtable
+        if(s.length() != t.length()){
+            return false;
+        }
+        int arr[26] = {0};
+        for(int i=0; i<s.length(); i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
 
-        return (s==t);
-
-        
     }
 };
+/*
+Tc : O(N), as we iterate the string to count the frequency of every character 
+    Sc : O(1), As we are using  26 size-frequency array to store the frequency of every character which is constant
+    */
