c735c69e2f386d27a0280d5a24ddb61561f75172
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-20 02:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (59.87%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        if(s.length()!=goal.length()) return false ;
+        string combine = s + s ;
+        if(combine.find(goal) == string::npos) return false ;
+
+        return true ;
+        
+    }
+};
