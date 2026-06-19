fbb9ae05605cf8ff2be50b4b31bd897a35c18e52
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-20 02:06:46 +0530

    Time: 5 ms (26.08%) | Memory: 9.6 MB (75.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //using String + Sorting
-        if(s.length() != t.length()){
-            return false;
-        }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t){
-            return true;  
-        }
-        return false;
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
+
+        return (s==t);
+
         
     }
 };
-
-/*
-
-    Time Complexity : O(NlogN + MlogM), Sorting the String(s and t) costs O(NlogN + MlogM). 
-
-    Space Complexity : O(1)
-
-     
-
-*/
