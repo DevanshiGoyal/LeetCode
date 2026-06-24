8c1f6631bec9cfd8e9870f8041ed94dfa784627b
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-25 02:19:22 +0530

    Time: 283 ms (39.47%) | Memory: 159.6 MB (27.89%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,106 +1,67 @@
-class Solution {
-
-// As 0,1 already occupied in matrix so we need to start with index = 2;
-int idx=2;
-
-// HashMap to have mapping between the connected component index and its area
-unordered_map<int,int> mp;
-
-// 4 directions to find out connected 1s
-vector<pair<int,int>> direction{{1,0},{-1,0},{0,1},{0,-1}};
-
-// Globally declaring the area variable which will store the max area
-int area = 0;
-
-// Simple DFS method for assigning connected components an index.
-void dfs(int i,int j,vector<vector<int>> &grid)
-{
-    if(i<0 or i>=size(grid) or j<0 or j>=size(grid) or grid[i][j]!=1)
-        return;
-    mp[idx]++;
-    grid[i][j]=idx;
-    for(auto dir:direction)
-    {
-        dfs(i+dir.first,j+dir.second,grid);
-    }
-}
-
-// Method to mark all the connected components.
-void markAllTheConnectedComponents(vector<vector<int>>& grid)
-{
-    int n=size(grid);
-    mp.clear();
-    idx=2;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(grid[i][j])
-            {
-                dfs(i,j,grid);
-            // Here we are handling the situation when there is no 0 in the matrix.
-                area=max(area,mp[idx]);
-                idx++;
-            }
-        }
-    }
-}
-
-// Method to find out the max area in case of any 0 to be flipped.
-void findMaxArea(vector<vector<int>>& grid)
-{
-    int n=size(grid);
-    queue<pair<int,int>> q;
-    
-//     Push all the {i,j} where grid[i][j]==0 in queue.
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(grid[i][j]==0)
-            {
-                q.push({i,j});
-            }
-        }
-    }
-
-    auto isValid = [&](int i,int j){
-        return (i>=0 and i<n and j>=0 and j<n and grid[i][j]!=0);
-    };
-
-    while(!q.empty())
-    {
-        auto front = q.front();
-        q.pop();
-        int i = front.first;
-        int j = front.second;
-        
-        unordered_set<int> vis;
-        int curr = 0;
-        for(auto dir:direction)
-        {
-            int newi = i+dir.first;
-            int newj = j+dir.second;
-            
-            if(isValid(newi,newj) and !vis.count(grid[newi][newj]))
-            {
-                vis.insert(grid[newi][newj]);
-                curr += mp[grid[newi][newj]];
-            }
-        }
-//         its curr+1, why? because we need to consider the flipped 0 also.
-        area = max(curr+1,area);
-    }
-}
-
-
+class DisjointSet {
 public:
-    int largestIsland(vector<vector<int>>& grid) 
-    {
-//         Step 1: Mark all the connected componentes with index.
-        markAllTheConnectedComponents(grid);
-//         Step 2: Do BFS to find which 0 flip will result in the larger area.
-        findMaxArea(grid);
-        return area;
+    vector<int> parent, size;
+    DisjointSet(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    int find(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = find(parent[i]);
+    }
+    void unite(int i, int j) {
+        int rootI = find(i), rootJ = find(j);
+        if (rootI != rootJ) {
+            if (size[rootI] < size[rootJ]) swap(rootI, rootJ);
+            parent[rootJ] = rootI;
+            size[rootI] += size[rootJ];
+        }
+    }
+};
+
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        DisjointSet ds(n * n);
+        int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
+
+        for (int r = 0; r < n; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] == 1) {
+                    for (int i = 0; i < 4; ++i) {
+                        int nr = r + dr[i], nc = c + dc[i];
+                        if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 1) {
+                            ds.unite(r * n + c, nr * n + nc);
+                        }
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n * n; ++i) ans = max(ans, ds.size[ds.find(i)]);
+
+        for (int r = 0; r < n; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] == 0) {
+                    unordered_set<int> seen;
+                    int cur = 1;
+                    for (int i = 0; i < 4; ++i) {
+                        int nr = r + dr[i], nc = c + dc[i];
+                        if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 1) {
+                            int root = ds.find(nr * n + nc);
+                            if (seen.find(root) == seen.end()) {
+                                cur += ds.size[root];
+                                seen.insert(root);
+                            }
+                        }
+                    }
+                    ans = max(ans, cur);
+                }
+            }
+        }
+        return ans;
     }
 };
