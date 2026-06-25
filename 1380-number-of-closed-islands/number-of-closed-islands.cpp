f9292bc7c102fc22d2a12d691a4bc300091dfc00
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-26 01:24:00 +0530

    Time: 23 ms (5.02%) | Memory: 26.1 MB (5.16%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -3,11 +3,11 @@ public:
     int closedIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<bool>> visit(m, vector<bool>(n));
         int count = 0;
+        vector<vector<bool>> visit(m, vector<bool>(n));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 && !visit[i][j] && bfs(i, j, m, n, grid, visit)) {
+                if (grid[i][j] == 0 && !visit[i][j] && dfs(i, j, m, n, grid, visit)) {
                     count++;
                 }
             }
@@ -15,30 +15,24 @@ public:
         return count;
     }
 
-    bool bfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
-        queue<pair<int, int>> q;
-        q.push({x, y});
-        visit[x][y] = 2;
+    bool dfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
+        if (x < 0 || x >= m || y < 0 || y >= n) {
+            return false;
+        }
+        if (grid[x][y] == 1 || visit[x][y]) {
+            return true;
+        }
+
+        visit[x][y] = true;
         bool isClosed = true;
+        vector<int> dirx {0, 1, 0, -1};
+        vector<int> diry {-1, 0, 1, 0};
 
-        vector<int> dirx{0, 1, 0, -1};
-        vector<int> diry{-1, 0, 1, 0};
-
-        while (!q.empty()) {
-            x = q.front().first;   // row nnumber
-            y = q.front().second;  // column number
-            q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int r = x + dirx[i];
-                int c = y + diry[i];
-                if (r < 0 || r >= m || c < 0 || c >= n) {
-                    // (x, y) is a boundary cell.
-                    isClosed = false;
-                } else if (grid[r][c] == 0 && !visit[r][c]) {
-                    q.push({r, c});
-                    visit[r][c] = true;
-                }
+        for (int i = 0; i < 4; i++) {
+            int r = x + dirx[i];
+            int c = y + diry[i];
+            if (!dfs(r, c, m, n, grid, visit)) {
+                isClosed = false;
             }
         }
 
