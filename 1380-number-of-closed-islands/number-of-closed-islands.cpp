b5558aac7eada62bc70c56c11c9f59a91067ae27
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-26 01:23:30 +0530

    Time: 3 ms (44.94%) | Memory: 18.4 MB (9.78%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/number-of-closed-islands.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,1,0,-1};
-    
     int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector <vector <int>> visited(m, vector <int>(n,0));
-        
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visit(m, vector<bool>(n));
         int count = 0;
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(grid[i][j]==0 and visited[i][j]==0)
-                    if(dfs(i,j,grid,visited)) count++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0 && !visit[i][j] && bfs(i, j, m, n, grid, visit)) {
+                    count++;
+                }
             }
         }
         return count;
     }
-protected:
-    bool isvalid(int x, int y, int m, int n, vector <vector <int>>&grid, vector <vector <int>>&visited){
-        if(visited[x][y] or grid[x][y]) return false;
-        return true;
-    }
-    bool dfs(int x, int y, vector <vector <int>>&grid, vector <vector <int>>&visited){
-        if(!isvalid(x,y,grid.size(),grid[0].size(),grid,visited)) return true;
-        
-        if(x==grid.size()-1 or x==0 or y==grid[0].size()-1 or y==0) return false;
-        
-        visited[x][y] = 1;
-        
-        bool left = dfs(x,y-1,grid,visited);
-        bool right = dfs(x,y+1,grid,visited);
-        bool up = dfs(x-1,y,grid,visited);
-        bool down = dfs(x+1,y,grid,visited);
-        
-        return left and right and up and down;
+
+    bool bfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        visit[x][y] = 2;
+        bool isClosed = true;
+
+        vector<int> dirx{0, 1, 0, -1};
+        vector<int> diry{-1, 0, 1, 0};
+
+        while (!q.empty()) {
+            x = q.front().first;   // row nnumber
+            y = q.front().second;  // column number
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int r = x + dirx[i];
+                int c = y + diry[i];
+                if (r < 0 || r >= m || c < 0 || c >= n) {
+                    // (x, y) is a boundary cell.
+                    isClosed = false;
+                } else if (grid[r][c] == 0 && !visit[r][c]) {
+                    q.push({r, c});
+                    visit[r][c] = true;
+                }
+            }
+        }
+
+        return isClosed;
     }
 };
-/*
-[[0,0,1,1,0,1,0,0,1,0],[1,1,0,1,1,0,1,1,1,0],[1,0,1,1,1,0,0,1,1,0],[0,1,1,0,0,0,0,1,0,1],[0,0,0,0,0,0,1,1,1,0],[0,1,0,1,0,1,0,1,1,1],[1,0,1,0,1,1,0,0,0,1],[1,1,1,1,1,1,0,0,0,0],[1,1,1,0,0,1,0,1,0,1],[1,1,1,0,1,1,0,1,1,0]]
-[[1,1,0,1,1,1,1,1,1,1],[0,0,1,0,0,1,0,1,1,1],[1,0,1,0,0,0,1,0,1,0],[1,1,1,1,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,0],[0,0,0,0,1,1,0,0,0,0],[1,0,1,0,0,0,0,1,1,0],[1,1,0,0,1,1,0,0,0,0],[0,0,0,1,1,0,1,1,1,0],[1,1,0,1,0,1,0,0,1,0]]
-*/
