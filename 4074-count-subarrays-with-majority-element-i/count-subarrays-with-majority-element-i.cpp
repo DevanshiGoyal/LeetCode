de208bf483a49bfd6f45d0339ad72441096d3f17
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-06-25 15:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (20.26%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
-
-        // brute force approach 
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
-        long long ans = 0 ;
-        for(int i = 0 ; i<n ; i++){
-            int targetcount = 0;
-            for(int j = i ; j<n ; j++){
-                if(nums[j]== target){
-                    targetcount++;
-                }
+        long long cnt = 0;
 
-                int len = j-i+1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) nums[i] = 1;
+            else nums[i] = -1;
+        }
 
-                if(targetcount>len/2){
-                    ans++;
-                }
+        vector<int> pref(n);
+        pref[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + nums[i];
+        }
+
+        int shift = n;
+        vector<int> freq(2 * n + 1, 0);
+
+        freq[shift] = 1;
+
+        long long valid = 0;
+        int lastSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (pref[i] > lastSum) {
+                valid += freq[lastSum + shift];
+            } else {
+                valid -= freq[pref[i] + shift];
             }
 
+            cnt += valid;
+            freq[pref[i] + shift]++;
+            lastSum = pref[i];
         }
-        return ans ;
-        
+
+        return cnt;
     }
 };
-
-// tc-->O(n^2)    sc --->O(1)
