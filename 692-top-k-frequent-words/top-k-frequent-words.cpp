8e1bbffb927bcaaca0976202f4dc5ab499e17813
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-02 03:07:15 +0530

    Time: 3 ms (63.40%) | Memory: 17.6 MB (71.85%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,30 +1,40 @@
-struct Compare {
-    bool operator() (pair<int, string> a, pair<int, string> b) {
-        if(a.first == b.first)
-            return a.second > b.second;
-        else
-            return a.first < b.first;
-    }
-};
-
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> m;
-        for(int i=0; i<words.size(); i++)
-            m[words[i]]++;
-        
-        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> q;
-        for(auto p : m)
-            q.push({p.second, p.first});
-        
-        vector<string> ans;
-        while(k--) {
-            ans.push_back(q.top().second);
-            q.pop();
+
+        // Count the frequency of every word
+        unordered_map<string, int> freq;
+        for (string &word : words) {
+            freq[word]++;
         }
-        
+
+        // bucket[i] stores all words that appear exactly i times
+        vector<vector<string>> bucket(words.size() + 1);
+
+        for (auto &entry : freq) {
+            bucket[entry.second].push_back(entry.first);
+        }
+
+        vector<string> ans;
+
+        // Traverse buckets from highest frequency to lowest
+        for (int i = words.size(); i >= 1; i--) {
+
+            if (bucket[i].empty())
+                continue;
+
+            // Words with the same frequency should be in lexicographical order
+            sort(bucket[i].begin(), bucket[i].end());
+
+            for (string &word : bucket[i]) {
+                ans.push_back(word);
+
+                // Stop after we have collected k words
+                if (ans.size() == k)
+                    return ans;
+            }
+        }
+
         return ans;
     }
 };
-// O(n log(k)) time and O(n) extra space
