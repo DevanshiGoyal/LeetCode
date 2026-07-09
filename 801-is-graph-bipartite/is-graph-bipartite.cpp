cc1a1a333b23b67ffb2383c62cb33bb837af3e7f
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-10 00:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (78.72%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,58 +1,48 @@
-class Solution {
+//Union Find
+class UnionFind {
+private:
+    vector<int> parent;
+
 public:
-    bool checkBipartiteBFS(vector<vector<int>>& adj , int node , vector<int> &color , int currcolor){
-
-        queue<int> q ;
-
-        q.push(node) ;
-
-        color[node] = currcolor;
-
-        while(!q.empty()){
-            int u = q.front() ;
-            q.pop() ;
-
-            for(int& v : adj[u]){
-                if(color[v] == color[u]){
-                    return false ;
-                }
-
-                if(color[v] == -1){
-                    color[v] = 1-color[u] ;
-                    q.push(v) ;
-                }
-            }
-        } 
-
-        return true ;       
+    UnionFind(int size) {
+        parent.resize(size);
+        for (int i = 0; i < size; ++i)
+            parent[i] = i;
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        // using bfs 
 
-        int n = graph.size() ;
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]); // Path compression
+        return parent[x];
+    }
 
-        vector<int> color(n , -1) ;
-
-        // red = 1 
-        // green = 0 
-
-        for(int i = 0 ; i<n ; i++){
-            if(color[i] == -1){
-                if(checkBipartiteBFS(graph , i , color , 1 ) == false) return false ;
-            }
-        }
-
-        return true ;
-        
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px != py)
+            parent[px] = py;
     }
 };
 
-/*
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        UnionFind uf(n);
 
-TC
-BFS  O(V + E)
-= O(V + E)
+        for (int i = 0; i < n; ++i) {
+            if (graph[i].empty()) continue;
 
-SC
-color array + queue = O(V)
-*/
+            // All neighbors should belong to the same group, different from node i
+            for (int j = 1; j < graph[i].size(); ++j) {
+                uf.unite(graph[i][0], graph[i][j]);
+            }
+
+            // If i and its neighbor belong to same group → Not bipartite
+            if (uf.find(i) == uf.find(graph[i][0]))
+                return false;
+        }
+
+        return true;
+    }
+};
