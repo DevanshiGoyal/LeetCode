4c3652d9fc3d40daad346355f36841245e964127
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-09 15:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (12.40%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
+    void dfs(int node , vector<vector<int>>& adj , vector<int>&vis){
+        vis[node] = 1 ;
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-
-        int n = isConnected.size();
-
-        vector<bool> visited(n, false);
-
-        int provinces = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if (!visited[i]) {
-
-                provinces++;
-
-                queue<int> q;
-                q.push(i);
-
-                visited[i] = true;
-
-                while (!q.empty()) {
-
-                    int u = q.front();
-                    q.pop();
-
-                    for (int v = 0; v < n; v++) {
-
-                        if (isConnected[u][v] == 1 && !visited[v]) {
-
-                            visited[v] = true;
-                            q.push(v);
-                        }
-                    }
-                }
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it , adj , vis) ;
             }
         }
 
-        return provinces;
+    }
+
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size() ;
+
+        // adj list 
+        vector<vector<int>> adj(n) ;
+        for(int u = 0 ; u<n ; u++){
+            for(int v = 0 ; v<n ; v++){
+                if(isConnected[u][v]==1 && u!=v){
+                    adj[u].push_back(v) ;
+                    adj[v].push_back(u) ;
+                }
+            }
+           
+        }
+        int count = 0 ;
+        vector<int> vis(n , 0) ;
+        for(int i = 0  ; i<n ; i++){
+            if(!vis[i]){
+                count++;
+                dfs(i , adj , vis);
+                
+            }
+        }
+        return count ;
     }
 };
+
+// sc  ---> O(n) + O(n)  (visited array + rescursion stack space)
+// tc---->  O(n)  + o(v+2e) 
