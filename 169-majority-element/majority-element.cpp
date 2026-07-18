50d4b5fa4692b9d35b77f1ee965578b67537eac8
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-18 19:00:23 +0530

    Time: 7 ms (10.47%) | Memory: 28.3 MB (35.44%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // brute force approach 
-        // using hashmap
-
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        unordered_map<int , int> mp ;  // num , count
-
-        for(int i = 0  ; i<n ; i++){
-            mp[nums[i]]++;
-        }
-        int ans ;
-        for(auto e :mp){
-            int f = e.second ;
-            int num = e.first;
-            if(f>n/2) ans = num ;
-
-        }
-        return ans ;
-        
+        return nums[n/2];
     }
 };
