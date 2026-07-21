1100afccab09ea18ace3a4f6cb8a768d85f77918
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-21 14:12:43 +0530

    Time: 21 ms (64.57%) | Memory: 17.3 MB (84.73%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-       vector<vector<int>>ans;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<n;j++){
-                if(j!=i+1&&nums[j]==nums[j-1])continue;
-                int k=j+1;
-                int r=n-1;
-                while(k<r){
-                    long long sum=nums[i];
-                    sum+=nums[j];
+        // optimal approach 
+
+        int n = nums.size();
+        vector<vector<int>> ans ;
+        sort(nums.begin() , nums.end());
+
+        for(int i = 0 ; i<n ; i++){
+            if(i>0 && nums[i] ==nums[i-1]) continue ;
+            for(int j= i+1 ; j<n ; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue ;
+
+                int k = j+1 ;
+                int l = n-1 ;
+
+                while(k<l){
+                    long long sum = nums[i] + nums[j];
                     sum+=nums[k];
-                    sum+=nums[r];
-                    if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[r]};
+                    sum+=nums[l];
+
+                    if(sum >target){
+                        l--;
+                    }else if (sum<target){
+                        k++;
+                    }
+                    else{
+                        vector<int> temp = {nums[i] , nums[j] , nums[k], nums[l]};
                         ans.push_back(temp);
-                        k++;r--;
-                        while(k<r&&nums[k]==nums[k-1])k++;
-                        while(k<r&&nums[r]==nums[r+1])r--;
-                    }else if(sum<target)k++;
-                    else r--;
+                        k++ , l--;
+
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while( k<l &&  nums[l] == nums[l+1]) l--;
+                        
+                    }
                 }
             }
-        }return ans;
+        }
+
+        return ans ;
+        
     }
 };
+//tc-->O(n^2 * n) O(n^3)
+//sc-->O(1)
