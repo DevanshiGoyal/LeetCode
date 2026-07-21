dd111083f7a6bc823478b0fca0d0be75209865e9
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-22 02:12:18 +0530

    Time: 101 ms (31.25%) | Memory: 79.2 MB (41.96%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.size();
         int ones = 0;
-        for (char ch : s) {
-            if (ch == '1') ones++;
+        for(char c : s)
+            if(c == '1')
+                ones++;
+
+        s = "1" + s + "1";
+
+        int n = s.size();
+        int i = 0;
+
+        int ans = ones;
+
+        // Skip first 1's
+        while(i < n && s[i] == '1')
+            i++;
+
+        // Read first 0-block
+        int c10 = 0;
+        while(i < n && s[i] == '0'){
+            c10++;
+            i++;
         }
 
-        int l = 0, r = n - 1;
-        while (l < n && s[l] == '1') l++;
-        while (r >= 0 && s[r] == '1') r--;
+        while(i < n){
 
-        if (l > r) return ones;
-
-        vector<pair<int, char>> store;
-        int len = 1;
-        for (int i = l + 1; i <= r; i++) {
-            if (s[i] == s[i - 1]) {
-                len++;
+            // Read middle 1-block
+            int c11 = 0;
+            while(i < n && s[i] == '1'){
+                c11++;
+                i++;
             }
-            else {
-                store.push_back({len, s[i - 1]});
-                len = 1;
-            }
-        }
-        store.push_back({len, s[r]});
 
-        int MaxRange = 0;
-        for (int i = 0; i + 2 < store.size(); i += 2) {
-            int range = store[i].first + store[i + 2].first;
-            MaxRange = max(MaxRange, range);
+            if(c11 == 0)
+                break;
+
+            // Read right 0-block
+            int c20 = 0;
+            while(i < n && s[i] == '0'){
+                c20++;
+                i++;
+            }
+
+            if(c20 == 0)
+                break;
+
+            ans = max(ans, ones + c10 + c20);
+
+            // Slide the window
+            c10 = c20;
         }
-        return ones + MaxRange;
+
+        return ans;
     }
 };
