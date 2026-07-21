713f6ddb1fd0a78bfdb59ecc5713e6ac48df7e37
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-21 14:23:36 +0530

    Time: 1630 ms (20.19%) | Memory: 34.2 MB (99.51%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> preSumMap;
-        int sum = 0, cnt = 0;
-
-        preSumMap[0] = 1; // to count subarrays that start from index 0
-
-        for (int num : nums) {
-            sum += num;
-            cnt += preSumMap[sum - k]; // how many times prefix sum occ
-            preSumMap[sum]++;
+        // brute force approach 
+        int n = nums.size();
+        int count = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            int sum = nums[i] ;
+            if(sum==k){
+                count++;
+            }
+            for(int j = i+1 ; j<n ; j++){
+                sum += nums[j];
+                if(sum==k){
+                    count++;
+                }
+            }
         }
-
-        return cnt;
+        return count ;
+        
     }
 };
+/*
+Time Complexity --> O(n ^ 2) // where n is the size of the array
+Space Complexity --> O(1) // we are not using anything extra from our side
+It paases [ 85 / 89 ] in built test cases
+*/
