9eb411d3b456a8ec33387b6a272f20834c6d7c5f
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-23 11:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (89.47%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int n=dungeon.size();
-        int m=dungeon[0].size();
+    int m,n;
+    int dp[201][201];
+    int solve(int i,int j,vector<vector<int>>&dungeon){
+        if(i==m || j==n) return 1e9;
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return hp_finder(dungeon,n,m,0,0,dp);
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(i==m-1 && j==n-1){
+            if(dungeon[i][j]>0){
+                return 1;
+            } else{
+                return abs(dungeon[i][j])+1;
+            }
+        }
+        int down=solve(i+1,j,dungeon);
+        int right=solve(i,j+1,dungeon);
+
+        int ans=min(down,right)-dungeon[i][j];
+
+        return dp[i][j]=ans>0?ans:1;
     }
-
-    int hp_finder(vector<vector<int>>&arr,int n,int m,int row,int col,vector<vector<int>>&dp){
-        if(row==n-1 && col==m-1){
-            return max(1,1-arr[row][col]);
-        }
-
-        if(row>=n || col>=m){
-            return INT_MAX;
-        }
-
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-
-        int right=hp_finder(arr,n,m,row,col+1,dp);
-        int down=hp_finder(arr,n,m,row+1,col,dp);
-
-        dp[row][col]=min(right,down)-arr[row][col];
-
-        dp[row][col]=max(1,dp[row][col]);
-
-        return dp[row][col];
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        m=dungeon.size(),n=dungeon[0].size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,dungeon);
     }
 };
