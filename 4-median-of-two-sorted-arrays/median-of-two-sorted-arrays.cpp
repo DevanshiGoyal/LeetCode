26fefa263ed311450cd08d39368fe35baaff05b7
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-26 01:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (95.12%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-           int n1 = a.size(), n2 = b.size();
-    //if n1 is bigger swap the arrays:
-    if (n1 > n2) return findMedianSortedArrays(b, a);
-
-    int n = n1 + n2; //total length
-    int left = (n1 + n2 + 1) / 2; //length of left half
-    //apply binary search:
-    int low = 0, high = n1;
-    while (low <= high) {
-        int mid1 = (low + high) >> 1;
-        int mid2 = left - mid1;
-        //calculate l1, l2, r1 and r2;
-        int l1 = INT_MIN, l2 = INT_MIN;
-        int r1 = INT_MAX, r2 = INT_MAX;
-        if (mid1 < n1) r1 = a[mid1];
-        if (mid2 < n2) r2 = b[mid2];
-        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
-        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
-
-        if (l1 <= r2 && l2 <= r1) {
-            if (n % 2 == 1) return max(l1, l2);
-            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int ct=0;
+        int i=0,j=0;
+        int n=n1+n2;
+        int midr=n/2;
+        int midl=midr-1;
+        int val1=-1,val2=-1;
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]){
+                if(ct==midl)val1=nums1[i];
+                if(ct==midr)val2=nums1[i];
+                ct++;
+                i++;
+            }
+            else{
+                if(ct==midl)val1=nums2[j];
+                if(ct==midr)val2=nums2[j];
+                ct++;
+                j++;
+            }
+            
         }
-
-        //eliminate the halves:
-        else if (l1 > r2) high = mid1 - 1;
-        else low = mid1 + 1;
+        while (i < n1) {
+        if (ct == midl)val1 = nums1[i];
+        if (ct == midr)val2 = nums1[i];
+        ct++;
+        i++;
     }
-    return 0; //dummy statement
+    while (j < n2) {
+        if (ct == midl)val1 = nums2[j];
+        if (ct == midr)val2 = nums2[j];
+        ct++;
+        j++;
+    }
+    if(n%2==0){
+        return (double)(val1+val2)/2;
+    }else{
+        return val2;
+    }
+
+        return -1;
     }
 };
-// 1 3 4 6 9 11 
-// 4 5 6 9 
-
-// BF-> TC->O(M+N) || SC->O(M+N)
