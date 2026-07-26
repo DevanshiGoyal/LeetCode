e3e6a0f26f261d5245c65f69957cdbe92120774e
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-26 19:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (33.59%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,20 +1,93 @@
+/*
+
+    Time Complexity : O(N*M), where N is the number of row and M is number of column of matrix(mat). Here two
+    nested loops create time complexity because in worst case we will visit all the cells of the matrix(mat).
+
+    Space Complexity : O(1), Constant space.
+
+    Solved using Matrix.
+
+*/
+
+
+/***************************************** Approach 1 *****************************************/
+/*
 class Solution {
+private: 
+    bool isPeakElement(vector<vector<int>>& mat, int row, int col){
+        int n = mat.size();
+        int m = mat[0].size();
+        int dxdy[][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for(int i=0; i<4; i++){
+            int newrow = row + dxdy[i][0];
+            int newcol = col + dxdy[i][1];
+            if(newrow >= 0 && newcol >= 0 && newrow < n && newcol < m && mat[newrow][newcol] > mat[row][col]){
+                return false;
+            }
+        }
+        return true;
+    }
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-
-        vector<int> ans(2);
-        int maxi = INT_MIN ;
-
-        for(int i = 0 ; i<mat.size() ; i++){
-            for(int j = 0 ; j<mat[0].size() ; j++){
-                if(mat[i][j] > maxi){
-                    maxi =  mat[i][j] ;
-                    ans[0] = i ;
-                    ans[1] = j ;
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(isPeakElement(mat, i, j)){
+                    return {i, j};
                 }
             }
         }
+        return {-1, -1};
+    }
+};
 
-        return ans ;
+
+*/
+
+
+
+/*
+
+    Time Complexity : O(NlogM), where N is the number of row and M is number of column of matrix(mat). Here we
+    are doing N work logM times which creates the time complexity.
+
+    Space Complexity : O(1), Constant space.
+
+    Solved using Matrix + Binary Search.
+
+*/
+
+
+/***************************************** Approach 2 *****************************************/
+
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0, high = m-1;
+        while(low <= high){
+            int maxRow = 0;
+            int midCol = (low+high) >> 1;
+            for(int row=0; row<n; row++){
+                if(mat[row][midCol] > mat[maxRow][midCol]){
+                    maxRow = row;
+                }
+            }
+            int currElement =  mat[maxRow][midCol];
+            int leftElement = midCol == 0 ? -1 : mat[maxRow][midCol-1];
+            int rightElement = midCol == m-1 ? -1 : mat[maxRow][midCol+1];
+            if(currElement > leftElement && currElement > rightElement){
+                return {maxRow, midCol};
+            }
+            else if(currElement < leftElement){
+                high = midCol - 1;
+            }
+            else{
+                low = midCol + 1;
+            }
+        }
+        return {-1, -1};
     }
 };
