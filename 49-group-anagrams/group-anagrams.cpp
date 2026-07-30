a352c941dd73e38217239846dcc55b25dbe3291f
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-07-30 13:08:15 +0530

    Time: 68 ms (10.34%) | Memory: 33.5 MB (12.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
+    // optimal approachh 
+    string encode(string& s){
+        vector<int> freq(26 , 0);
+        for(auto& ch : s){
+            freq[ch-'a']++;
+        }
+        string encoded = "";
+        for(int i = 0 ; i<26 ; i++){
+            encoded+=char('a' + i) + to_string(freq[i]);
+        }
+        return encoded ;
+    }
+
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans ;
-
-        unordered_map<string , vector<string>> mp ;
-        for(int i = 0 ; i<strs.size() ; i++){
-            string s = strs[i];
-            sort(strs[i].begin() , strs[i].end());
-            mp[strs[i]].push_back(s);
-        }
-
-        for(auto it : mp){
-            ans.push_back(it.second);
-
-        }
-        return ans ;
-
-        
-        
+        unordered_map<string, vector<string>> mp;
+        for (auto& s : strs)
+            mp[encode(s)].push_back(s);
+        vector<vector<string>> ans;
+        for (auto& [key, vec] : mp)
+            ans.push_back(vec);
+        return ans;
+    
+               
     }
 };
+//O(n × k)	      sc --- O(n × k)
 
 /*
-Time complexity: O(m∗nlogn)
-Space complexity: O(mn)
+
+Dry Run
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+String	Encoded Key	Group
+"eat"	a1...e1...t1...	Group A
+"tea"	a1...e1...t1...	Group A
+"tan"	a1...n1...t1...	Group B
+"ate"	a1...e1...t1...	Group A
+"nat"	a1...n1...t1...	Group B
+"bat"	a1...b1...t1...	Group C
+Output: [["eat","tea","ate"], ["tan","nat"], ["bat"]]
 */
