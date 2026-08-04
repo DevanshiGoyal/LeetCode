3a52a28276fbd9ca59643029c3ef04d208d94ac4
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-05 01:03:09 +0530

    Time: 14 ms (90.94%) | Memory: 139.2 MB (71.19%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,44 +1,28 @@
-/*
-using deque approach   ---> montonic decreasing 
-1  When new ele comes nums[i] , make space fo fit (window size can't be greater than k )
-2  when nums[i] > deque ele no need to keep small ele in that window , pop them
-
-3  push i in deque ---> for nums[i]
-
-4  if(i>= k-1) , deq.front() is oue ans for that window 
-
-*/
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size() ;
+        // brute force - TLE
+        vector<int> ans ;
+        
+        deque<int> dq ;  // store indices 
 
-        deque<int> deq;
-
-        vector<int> result ;
-
-        for(int i = 0 ; i<n ; i++){
-            // make space for nums[i] 
-            // <=i-k
-            while(!deq.empty() && deq.front() <= i-k){
-                deq.pop_front() ;
-            }
-            // no need to keep small ele --> monotonic decreasing 
-            while(!deq.empty() && nums[i] >= nums[deq.back()]){
-                deq.pop_back();
-
-            }
-            deq.push_back(i) ;
-
-            // add max for current window
-            if(i>=k-1){
-                result.push_back(nums[deq.front()]) ;
+        for(int i = 0 ; i<nums.size() ;i++){
+            if(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
             }
 
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            if(i>= k-1){
+                ans.push_back(nums[dq.front()]);
+            }
         }
-        return result ;        
+
+        return ans ;
         
     }
 };
-
-// tc-->o(N)  sc--> O(k)
