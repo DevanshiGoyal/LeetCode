3807b61670af0a204f4b86509c491fbbe0aaf35d
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-09 21:19:06 +0530

    Time: 48 ms (46.34%) | Memory: 45.6 MB (39.86%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n =  nums.size() ;
-        int prefix[n]  ;
-        prefix[0] = nums[0];
-        for(int i = 1 ; i<n ; i++){
-            prefix[i] = prefix[i-1]+nums[i];
+        unordered_map<int, int> mp;
+
+        mp[0] = 1;
+
+        int prefix = 0;
+        int ans = 0;
+
+        for (int x : nums) {
+            prefix += x;
+
+            if (mp.find(prefix - k) != mp.end()) {
+                ans += mp[prefix - k];
+            }
+
+            mp[prefix]++;
         }
 
-        unordered_map<int , int>mp ;
-        int ans = 0 ;
-
-        for(int i = 0 ; i<n ; i++){
-            if(prefix[i] == k){
-                ans++;
-            }
-            if(mp.find(prefix[i] - k) != mp.end()){
-                ans+=mp[prefix[i] - k];
-            }
-            mp[prefix[i]]++;
-        }
-        
-        return ans ;
+        return ans;
     }
 };
-
-/*
-Time Complexity --> O(n) // where n is the size of the array
-Space Complexity --> O(n) // we are using unordered map from our side
-It paases [ 89 / 89 ] in built test cases
-*/
