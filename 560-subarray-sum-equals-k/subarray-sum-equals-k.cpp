b7f0076d67aca5275b597c42028cdec254a923c2
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-09 21:18:29 +0530

    Time: 32 ms (92.90%) | Memory: 45.4 MB (55.80%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // brute force approach 
-        int n = nums.size();
-        int count = 0 ;
-        for(int i = 0 ; i<n ; i++){
-            int sum = nums[i] ;
-            if(sum==k){
-                count++;
-            }
-            for(int j = i+1 ; j<n ; j++){
-                sum += nums[j];
-                if(sum==k){
-                    count++;
-                }
-            }
+        int n =  nums.size() ;
+        int prefix[n]  ;
+        prefix[0] = nums[0];
+        for(int i = 1 ; i<n ; i++){
+            prefix[i] = prefix[i-1]+nums[i];
         }
-        return count ;
+
+        unordered_map<int , int>mp ;
+        int ans = 0 ;
+
+        for(int i = 0 ; i<n ; i++){
+            if(prefix[i] == k){
+                ans++;
+            }
+            if(mp.find(prefix[i] - k) != mp.end()){
+                ans+=mp[prefix[i] - k];
+            }
+            mp[prefix[i]]++;
+        }
         
+        return ans ;
     }
 };
+
 /*
-Time Complexity --> O(n ^ 2) // where n is the size of the array
-Space Complexity --> O(1) // we are not using anything extra from our side
-It paases [ 85 / 89 ] in built test cases
+Time Complexity --> O(n) // where n is the size of the array
+Space Complexity --> O(n) // we are using unordered map from our side
+It paases [ 89 / 89 ] in built test cases
 */
