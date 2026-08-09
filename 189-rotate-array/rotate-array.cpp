3b11bf531e407c78721af267b7414822ed07182b
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-09 12:07:21 +0530

    Time: 3 ms (7.80%) | Memory: 29.5 MB (70.66%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+/*
+Time Complexity: O(n) (each element reversed at most twice)
+Space Complexity: O(1)
+
+*/
+
+
