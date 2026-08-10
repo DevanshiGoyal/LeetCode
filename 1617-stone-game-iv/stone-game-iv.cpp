cde105bbef44eeaf2276c2590685078cb863e39d
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-11 03:57:24 +0530

    Time: 35 ms (71.31%) | Memory: 7.9 MB (97.24%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,30 +1,32 @@
-int dp[100001][2];
+bool dp[100001][2];
 
 class Solution {
 public:
-    bool f(int n, bool turn) {
-        if(n == 0) {
-            if(turn) return false;
-            return true;
-        }
-        if(dp[n][turn] != -1) return dp[n][turn];
-        bool alice = false;
-        if(turn) {
-            for(int i = 1; i*i <= n; i++) {
-                alice |= f(n - i*i, false);
+    bool winnerSquareGame(int N) {
+        memset(dp, false, sizeof(dp));
+        dp[0][0] = true;
+        for(int n = 1; n <= N; n++) {
+            for(int turn = 0; turn <= 1; turn++) {
+                bool alice = false;
+                if(turn) {
+                    for(int i = 1; i*i <= n; i++) {
+                        alice |= dp[n - i*i][false];
+                        if(alice) break;
+                    }
+                }
+                else {
+                    alice = true;
+                    for(int i = 1; i*i <= n; i++) {
+                        bool bob = dp[n - i*i][true];
+                        if(!bob) {
+                            alice = bob;
+                            break;
+                        }
+                    }
+                }
+                dp[n][turn] = alice;
             }
         }
-        else {
-            alice = true;
-            for(int i = 1; i*i <= n; i++) {
-                bool bob = f(n - i*i, true);
-                if(!bob) return dp[n][turn] = bob;
-            }
-        }
-        return dp[n][turn] = alice;
-    }
-    bool winnerSquareGame(int n) {
-        memset(dp, -1, sizeof(dp));
-        return f(n, true);
+        return dp[N][true];
     }
 };
