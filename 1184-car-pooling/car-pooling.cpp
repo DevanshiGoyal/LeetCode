2732ecb6a15c21b42c88d8b36b9d4d48507a32d3
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-11 18:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (39.50%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.cpp
@@ -0,0 +1,29 @@
+//Approach-1 (Using Line Sweep)
+//T.C : O(n)
+//S.C : O(n) to store events in map
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int, int> events;
+
+        for(auto &trip : trips) {
+            int count = trip[0];
+            int start = trip[1];
+            int end   = trip[2];
+
+            events[start] += count;
+            events[end] -= count;
+        }
+
+        int passCount = 0;
+
+        for(auto &it : events) {
+            passCount += it.second;
+
+            if(passCount > capacity)
+                return false;
+        }
+
+        return true;
+    }
+};
