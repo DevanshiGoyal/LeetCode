d516612f2ad3783a141ea9c203b1c50b75db9221
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-11 20:01:24 +0530

    Time: 106 ms (27.22%) | Memory: 117 MB (37.36%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        int n = nums.size();
+        unordered_set<int> set;
         
-        for(int i=0; i<n; i++)
-        {
-            // mp.count() will tell whatever ith index that I want, have I seen it before?
-            if(mp.count(nums[i]))
-            {
-                // if I have already seen this number, then check for condition abs(i - j) <= k
-                if(abs(i-mp[nums[i]])<=k)
-                    return true;
+        for (int i = 0; i < nums.size(); ++i) {
+            // Remove the element that is now outside the window
+            if (i > k) {
+                set.erase(nums[i - k - 1]);
             }
-            // if I have not seen this number before, insert the number with its position in the map
-            // and if the number is already present in the map, then update the position of that number
-            mp[nums[i]] = i;
+            
+            // Check if the current element exists in the set
+            if (set.count(nums[i])) {
+                return true;
+            }
+            
+            // Add the current element to the set
+            set.insert(nums[i]);
         }
-        // after the complete traversal, if we don't find a pair to satisfy the condition, return false
+        
         return false;
+        
     }
 };
