7dd7428d1fe8a05d5d3449b12ea63320b811e3b7
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-14 14:27:05 +0530

    Time: 7 ms (38.23%) | Memory: 23.8 MB (59.62%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,44 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& v) {
-        //TC-O(NlogN + 2N)
-        //SC-O(N)
-        /*int n=v.size();
-        sort(v.begin(),v.end());
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end());
+
         vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
 
-        for(int i=0;i<n;i++)
-        {
-            int s=v[i][0];
-            int e=v[i][1];
+        for (int i = 1; i < intervals.size(); i++) {
 
-            if(!ans.empty() && e<=ans.back()[1])
-            continue;
-
-            for(int j=i+1;j<n;j++)
-            {
-                if(v[j][0] <= e)
-                e=max(e,v[j][1]);
-
-                else
-                break;
+            if (intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
             }
-        ans.push_back({s,e});
         }
-    return ans;*/
-    //TC-O(NlogN + N)
-    //SC-O(N)
-    int n=v.size();
-    sort(v.begin(),v.end());
-    vector<vector<int>> ans;
 
-    for(int i=0;i<n;i++)
-    {
-        if(ans.empty() || v[i][0]>ans.back()[1])
-        ans.push_back(v[i]);
-        else
-        ans.back()[1]=max(ans.back()[1],v[i][1]);
-    }
-    return ans;
+        return ans;
     }
 };
