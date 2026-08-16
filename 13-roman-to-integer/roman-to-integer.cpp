0a48155d40ec64f34476d650746f6b0a289d23f3
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-17 01:44:57 +0530

    Time: 3 ms (75.83%) | Memory: 10 MB (87.15%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-
     int priority(char c){
-        if(c=='I') return 1;
-        if(c=='V') return 5;
-        if(c=='X') return 10;
-        if(c=='L') return 50;
+        if(c=='I') return 1 ;
+        if(c=='V') return 5 ;
+        if(c== 'X') return 10 ;
+        if(c=='L') return 50 ;
         if(c=='C') return 100;
         if(c=='D') return 500;
         if(c=='M') return 1000;
-        return 0;
+        else return 0 ;
     }
-
     int romanToInt(string s) {
-        int sol = 0;
-        for(int i=1; i<s.size(); i++){
-            // largest to smallest 
-            if(priority(s[i-1])>=priority(s[i])) sol += priority(s[i-1]);
-            else sol -= priority(s[i-1]);
-        }  
-        sol += priority(s[s.size()-1]);
-        return sol;      
+        int sol = 0 ;
+        for(int i = 1; i<s.length() ; i++){
+            if(priority(s[i-1])>=priority(s[i])){
+                sol+=priority(s[i-1]);
+            }else{
+                sol-= priority(s[i-1]); 
+            }
+        }
+        sol+=priority(s[s.size()-1]);
+        return sol ;
+        
     }
-
-    
 };
-//TC-->O(N)  SC-->O(1)
