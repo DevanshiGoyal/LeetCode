7ee91f4508aaf289a6c57a93b4a7afa3e3921de4
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-22 03:33:08 +0530

    Time: 15 ms (35.84%) | Memory: 11.5 MB (43.55%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,54 +1,36 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-
-        int n = s.length();
-        int m = t.length();
-
-        if (m > n)
-            return "";
-
-        vector<int> hash(256, 0);
-
-        for (char ch : t)
-            hash[ch]++;
-
-        int l = 0, r = 0;
-        int cnt = 0;
-
-        int startIndex = -1;
-        int mini = INT_MAX;
-
-        while (r < n) {
-
-            if (hash[s[r]] > 0)
-                cnt++;
-
-            hash[s[r]]--;
-
-            while (cnt == m) {
-
-                if (r - l + 1 < mini) {
-                    mini = r - l + 1;
-                    startIndex = l;
-                }
-
-                hash[s[l]]++;
-
-                if (hash[s[l]] > 0)
-                    cnt--;
-
-                l++;
-            }
-
-            r++;
+        map<char, int> freq;
+        int left = 0;
+        int right = 0;
+        int minlength = INT_MAX;
+        int count = 0;
+        int sIndex = -1;
+        for (int i = 0; i < t.size(); i++) {
+            freq[t[i]]++;
         }
-
-        return startIndex == -1 ? "" : s.substr(startIndex, mini);
+        while (right < s.size()) {
+            if (freq[s[right]] > 0) {
+                count++;
+            }
+            freq[s[right]]--;
+            right++;
+            while (count == t.size()) {
+                if (right - left < minlength) {
+                    minlength = right - left;
+                    sIndex = left;
+                }
+                freq[s[left]]++;
+                if (freq[s[left]] > 0) {
+                    count--;
+                }
+                left++;
+            }
+        }
+        if (sIndex == -1) {
+            return "";
+        }
+        return s.substr(sIndex, minlength);
     }
 };
-
-/*
-Time complexity: O(2n+m)
-Space complexity: O(256)
-*/
