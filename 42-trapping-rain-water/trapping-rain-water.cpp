4be076bb1092f4400b16bfdff1d5292529588b37
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-24 03:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (16.67%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,13 +1,42 @@
 class Solution {
 public:
-    //total water is trapped into the bars
-    int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
-        while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+
+    vector<int> getLeftMaxArray(vector<int>&height , int& n){
+        vector<int> leftMax(n) ;
+        leftMax[0] = height[0] ;
+
+        for(int i= 1 ; i<n ; i++){
+            leftMax[i] = max(leftMax[i-1] , height[i]);
         }
-        return ans;
+        return leftMax;
+
+    }
+    vector<int> getRightMaxArray(vector<int>&height , int& n){
+        vector<int> rightMax(n) ;
+        rightMax[n-1] = height[n-1] ;
+
+        for(int i= n-2 ; i>=0 ; i--){
+            rightMax[i] = max(rightMax[i+1] , height[i]);
+        }
+        return rightMax;
+        
+    }
+    int trap(vector<int>& height) {
+        int n = height.size() ;
+        vector<int> leftMax = getLeftMaxArray(height , n) ;
+        vector<int> rightMax = getRightMaxArray(height , n) ;
+
+        int sum = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            //int width = 1 ;
+            int h = min(leftMax[i] , rightMax[i]) - height[i] ;
+
+            sum+=(1 * h) ;
+
+        }
+        return sum ;
+        
     }
 };
+
+// tc-->O(n) sc-->O(n)
