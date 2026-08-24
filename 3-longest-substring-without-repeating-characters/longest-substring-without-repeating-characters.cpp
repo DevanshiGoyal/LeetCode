de64db669541bfd30bb1b873cbb367d096952712
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-08-25 03:30:40 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (39.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen = 0;
-        int l = 0, r = 0;
-        int n = s.length();
 
-        unordered_set<char> charSet;
+        vector<int> lastIndex(256, -1);
 
-        while (r < n) {
-            if (charSet.find(s[r]) == charSet.end()) {
-                // Character not seen before, expand window
-                charSet.insert(s[r]);
-                maxlen = max(maxlen, r - l + 1);
-                r++;
-            } else {
-                // Duplicate found, shrink window from the left
-                charSet.erase(s[l]);
-                l++;
+        int l = 0;
+        int ans = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+
+            if (lastIndex[s[r]] >= l) {
+                l = lastIndex[s[r]] + 1;
             }
+
+            lastIndex[s[r]] = r;
+
+            ans = max(ans, r - l + 1);
         }
 
-        return maxlen;
+        return ans;
     }
 };
 
-//TC--->O(N)
-//SC--->O(N)
+//  O(n)    O(256)
