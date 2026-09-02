8a7e0b88bc24be67fe70ae5c26d07966be76285d
Devanshi Goyal <goyaldevanshi444@gmail.com>
2026-09-03 03:38:31 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.41%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        //It is always possible to make the array all odd with at least 1 odd number since even - odd = odd, if there are no odds the array is already all even so it is always possible, the answer is just return true
-        return true ;
-        
+        int n = nums1.size();
+        bool odd = true, even = true;
+
+        for(int i=0; i<n; i++){
+            if(nums1[i]%2==0){
+                // check for odd
+                bool valid = false;
+                for(int j=0; j<n; j++){
+                    if(i!=j && abs(nums1[i]-nums1[j])%2==1){
+                        valid = true;
+                    }
+                }
+                if(!valid) odd = false;
+            }
+            else {
+                // check for even
+                bool valid = false;
+                for(int j=0; j<n; j++){
+                    if(i!=j && abs(nums1[i]-nums1[j])%2==0){
+                        valid = true;
+                    }
+                }
+                if(!valid) even = false;
+            }
+        }
+
+        return odd | even;
     }
 };
